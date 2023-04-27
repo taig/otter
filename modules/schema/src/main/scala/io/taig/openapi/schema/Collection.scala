5a560e34a3c3858f2713842cd69dc437ecae7094
Niklas Klein <mail@taig.io>
2023-04-27 19:55:46 +0200

    Super smooth collection implementation

---
+++ modules/schema/src/main/scala/io/taig/openapi/schema/Collection.scala
@@ -0,0 +1,60 @@
+package io.taig.openapi.schema
+
+import cats.Eval
+import cats.data.{Chain, Validated}
+import cats.syntax.all.*
+import io.taig.openapi.OpenApi
+import io.taig.validation.{Constraint, Validation}
+
+abstract class Collection[A](
+    val constraints: Chain[Constraint[OpenApi]],
+    val metadata: Collection.Metadata[A],
+    val schema: Eval[Schema[?]]
+) extends Schema[A]:
+  self =>
+
+  type Of <: OpenApi
+  final override type Self[a] = Collection.Of[a, Of]
+  final override type Codec = OpenApi.Array[Of]
+  final override type Metadata[a] = Collection.Metadata[a]
+
+  final override def copy(metadata: Collection.Metadata[A]): Collection.Of[A, Of] =
+    new Collection[A](constraints, metadata, schema) { export self.{decode, encode, Of} }
+
+  final override def ivalidate[B](validation: Validation[A, A, A, B])(g: B => A): Collection.Of[B, Of] =
+    new Collection[B](
+      constraints ++ validation.constraints.map(_.map(self.encode)),
+      metadata.flatMap(validation.run(_).toOption),
+      schema
+    ):
+      override type Of = self.Of
+      override def decode(openapi: OpenApi.Array[?]): Validated[Violations, B] =
+        self.decode(openapi).andThen(andThenValidate(validation, self.encode))
+      override def encode(b: B): OpenApi.Array[self.Of] = self.encode(g(b))
+
+  final override def decode(openapi: OpenApi): Validated[Violations, A] = openapi match
+    case openapi: OpenApi.Array[?] => decode(openapi)
+    case openapi                   => typeViolations("Array", openapi).invalid
+
+  def decode(openapi: OpenApi.Array[?]): Validated[Violations, A]
+
+object Collection:
+  type Of[A, B <: OpenApi] = Collection[A] { type Of = B }
+
+  final case class Metadata[A](description: Option[String], example: Option[A]) extends Schema.Metadata[A]:
+    override type Self[a] = Collection.Metadata[a]
+    override def map[B](f: A => B): Collection.Metadata[B] = copy(example = example.map(f))
+    override def flatMap[B](f: A => Option[B]): Collection.Metadata[B] = copy(example = example.flatMap(f))
+    override def updated(description: Option[String], example: Option[A]): Collection.Metadata[A] =
+      Metadata(description, example)
+
+  object Metadata:
+    def empty[A]: Collection.Metadata[A] = Metadata(none, none)
+
+  def apply[A, B <: OpenApi](of: Eval[Schema.Of[A, B]]): Collection.Of[Vector[A], B] =
+    new Collection[Vector[A]](Chain.empty, Metadata.empty, of):
+      override type Of = B
+      override def decode(openapi: OpenApi.Array[?]): Validated[Violations, Vector[A]] =
+        openapi.toVector.zipWithIndex.traverse: (openapi, index) =>
+          of.value.decode(openapi).leftMap(_.modifyHistory(index /: _))
+      override def encode(as: Vector[A]): OpenApi.Array[B] = OpenApi.fromVector(as.map(of.value.encode))
