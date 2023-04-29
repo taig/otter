72250e0369878a4d36faee54cf8ff0df8e0b578d
Niklas Klein <mail@taig.io>
2023-04-29 13:25:19 +0200

    Bring back enumeration

--- modules/schema/src/main/scala/io/taig/openapi/schema/Enumeration.scala
+++ modules/schema/src/main/scala/io/taig/openapi/schema/Enumeration.scala
@@ -1,62 +1,78 @@
-//package io.taig.openapi.schema
-//
-//import cats.Eval
-//import cats.data.{Chain, Validated}
-//import cats.syntax.all.*
-//import io.taig.openapi.OpenApi
-//import io.taig.validation.{Constraint, Validation, Violation}
-//
-//sealed abstract class Enumeration[A](
-//    val constraints: Chain[Constraint[OpenApi]],
-//    val metadata: Enumeration.Metadata[A],
-//    val schema: Eval[Schema.Of[?, OpenApi.Primitive]],
-//    val values: Eval[Set[OpenApi.Primitive]]
-//) extends Value[A]:
-//  self =>
-//  final override type Self[a] = Enumeration[a]
-//  final override type Metadata[a] = Enumeration.Metadata[a]
-//
-//  final override def copy(metadata: Enumeration.Metadata[A]): Enumeration[A] =
-//    new Enumeration[A](constraints, metadata, schema, values) { export self.{decode, encode, parse, render} }
-//
-//  final override def ivalidate[B](validation: Validation[A, A, A, B])(g: B => A): Enumeration[B] = ???
-//
-//  final override def decode(openapi: OpenApi): Validated[Violations, A] = openapi match
-//    case openapi: OpenApi.Primitive => decode(openapi)
-//    case _                          => typeViolations("Primitive", openapi).invalid
-//
-//  def decode(openapi: OpenApi.Primitive): Validated[Violations, A]
-//
-//object Enumeration:
-//  final case class Metadata[A](description: Option[String], example: Option[A]) extends Schema.Metadata[A]:
-//    override type Self[a] = Enumeration.Metadata[a]
-//    override def map[B](f: A => B): Enumeration.Metadata[B] = copy(example = example.map(f))
-//    override def flatMap[B](f: A => Option[B]): Enumeration.Metadata[B] = copy(example = example.flatMap(f))
-//    override def updated(description: Option[String], example: Option[A]): Enumeration.Metadata[A] =
-//      Metadata(description, example)
-//
-//  object Metadata:
-//    def empty[A]: Enumeration.Metadata[A] = Metadata(None, None)
-//
-//  def apply[A, B](of: Eval[Value[A]], values: Set[B], mapping: B => A): Enumeration[B] =
-//    val lookup: Eval[A => Option[B]] = Eval.later(values.map(b => mapping(b) -> b).toMap.get(_))
-//    val openapis = of.map(schema => values.map(mapping).map(schema.encode))
-//
-//    new Enumeration[B](Chain.empty, Metadata.empty, of, openapis):
-//      override def decode(openapi: OpenApi.Primitive): Validated[Violations, B] =
-//        of.value
-//          .decode(openapi)
-//          .andThen: key =>
-//            Validated.fromOption(
-//              lookup.value(key), {
-//                val references = OpenApi.fromList(openapis.value.toList)
-//                val constraint = Constraint("enumeration", references.some)
-//                Violations.rootNec(Violation(constraint, of.value.encode(key)))
-//              }
-//            )
-//
-//      override def encode(b: B): OpenApi.Primitive = of.value.encode(mapping(b))
-//
-//      override def parse(value: String): Validated[Violations, B] = of.value.parse(value).andThen(???)
-//
-//      override def render(b: B): String = of.value.render(mapping(b))
+package io.taig.openapi.schema
+
+import cats.Eval
+import cats.data.{Chain, Validated}
+import cats.syntax.all.*
+import io.taig.openapi.OpenApi
+import io.taig.validation.{Constraint, Validation, Violation}
+
+sealed abstract class Enumeration[A] extends Value[A]:
+  self =>
+  final override type Self[a] = Enumeration[a]
+
+  def schema: Eval[Value[?]]
+
+  def values: Eval[Set[OpenApi.Primitive]]
+
+  final override def ivalidate[B](validation: Validation[A, A, A, B])(g: B => A): Enumeration[B] =
+    Enumeration.Validate(this, validation, g, example.flatMap(validation.run(_).toOption))
+
+  final override def decode(openapi: OpenApi): Validated[Violations, A] = openapi match
+    case openapi: OpenApi.Primitive => decode(openapi)
+    case _                          => typeViolations("Primitive", openapi).invalid
+
+  def decode(openapi: OpenApi.Primitive): Validated[Violations, A]
+
+object Enumeration:
+  final private case class Root[A, B](
+      description: Option[String],
+      example: Option[B],
+      mapping: B => A,
+      schema: Eval[Value[A]],
+      source: Set[B]
+  ) extends Enumeration[B]:
+    val lookup: Eval[A => Option[B]] = Eval.later(source.map(b => mapping(b) -> b).toMap.get(_))
+
+    override def constraints: Chain[Constraint[OpenApi]] = Chain.empty
+    override def values: Eval[Set[OpenApi.Primitive]] = schema.map(schema => source.map(mapping).map(schema.encode))
+    override def modifyDescription(f: Option[String] => Option[String]): Enumeration[B] =
+      copy(description = f(description))
+    override def modifyExample(f: Option[B] => Option[B]): Enumeration[B] = copy(example = f(example))
+    override def decode(openapi: OpenApi.Primitive): Validated[Violations, B] = schema.value
+      .decode(openapi)
+      .andThen: key =>
+        Validated.fromOption(
+          lookup.value(key), {
+            val references = OpenApi.fromList(values.value.toList)
+            val constraint = Constraint("enumeration", references.some)
+            Violations.rootNec(Violation(constraint, schema.value.encode(key)))
+          }
+        )
+    override def encode(b: B): OpenApi.Primitive = schema.value.encode(mapping(b))
+    override def parse(value: String): Validated[Violations, B] =
+      schema.value.parse(value).andThen(a => lookup.value(a).toValid(???))
+    override def render(b: B): String = schema.value.render(mapping(b))
+
+  final private case class Validate[A, B](
+      enumeration: Enumeration[A],
+      validation: Validation[A, A, A, B],
+      g: B => A,
+      example: Option[B]
+  ) extends Enumeration[B]:
+    override def constraints: Chain[Constraint[OpenApi]] =
+      enumeration.constraints ++ validation.constraints.map(_.map(enumeration.encode))
+    override def description: Option[String] = enumeration.description
+    override def schema: Eval[Value[?]] = enumeration.schema
+    override def values: Eval[Set[OpenApi.Primitive]] = enumeration.values
+    override def modifyDescription(f: Option[String] => Option[String]): Enumeration[B] =
+      copy(enumeration = enumeration.modifyDescription(f))
+    override def modifyExample(f: Option[B] => Option[B]): Enumeration[B] = copy(example = f(example))
+    override def decode(openapi: OpenApi.Primitive): Validated[Violations, B] =
+      enumeration.decode(openapi).andThen(andThenValidate(validation, enumeration.encode))
+    override def encode(b: B): OpenApi.Primitive = enumeration.encode(g(b))
+    override def parse(value: String): Validated[Violations, B] =
+      enumeration.parse(value).andThen(andThenValidate(validation, enumeration.encode))
+    override def render(b: B): String = enumeration.render(g(b))
+
+  def apply[A, B](schema: Eval[Value[A]], values: Set[B], mapping: B => A): Enumeration[B] =
+    Root(none, none, mapping, schema, values)
