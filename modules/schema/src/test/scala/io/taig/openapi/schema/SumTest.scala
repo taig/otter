a4cc56d5498e337ac37699d5257bfeff98702f6a
Niklas Klein <mail@taig.io>
2023-04-30 21:16:42 +0200

    Sums

--- modules/schema/src/test/scala/io/taig/openapi/schema/SumTest.scala
+++ modules/schema/src/test/scala/io/taig/openapi/schema/SumTest.scala
@@ -9,10 +9,24 @@ final class SumTest extends FunSuite:
   enum Foo:
     case Bar(name: String)
 
-  test("as: enum 1") {
-    val bar = field("name", string).toProduct.as[Foo.Bar]
-    val foo = branch("bar", bar).toSum.as[Foo]
+  val bar: Product[String, Foo.Bar] = field("name", string).toProduct.as[Foo.Bar]
+  val foo: Sum[String, Foo] = branch("bar", bar).toSum.as[Foo]
 
+  enum Animal:
+    case Bird(name: String)
+    case Cat(lives: Int)
+    case Dog(goodBoy: Boolean)
+
+  val bird: Product[String, Animal.Bird] = field("name", string).toProduct.as[Animal.Bird]
+  val cat: Product[String, Animal.Cat] = field("lives", int).toProduct.as[Animal.Cat]
+  val dog: Product[String, Animal.Dog] = field("goodBoy", boolean).toProduct.as[Animal.Dog]
+  val animal: Sum[String, Animal] = (
+    branch("bird", bird) :+
+      branch("cat", cat) :+
+      branch("dog", dog)
+  ).as[Animal]
+
+  test("as: enum 1") {
     assertEquals(
       obtained = foo.encode(Foo.Bar("foobar")),
       expected = OpenApi.obj(
