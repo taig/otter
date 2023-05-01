7989d2ee20633f44bc55a9c64bc7f2473559b14b
Niklas Klein <mail@taig.io>
2023-05-02 00:11:54 +0200

    Headers fully implemented

--- modules/http/src/main/scala/io/taig/openapi/http/Headers.scala
+++ modules/http/src/main/scala/io/taig/openapi/http/Headers.scala
@@ -2,9 +2,11 @@ package io.taig.openapi.http
 
 import cats.data.Validated
 import cats.syntax.all.*
-import io.taig.openapi.OpenApi
+import io.taig.openapi.{Encoder, OpenApi}
 import io.taig.openapi.schema.Void
+import io.taig.openapi.schema.applyValidation
 import io.taig.openapi.schema.Violations
+import io.taig.openapi.validation.Validation
 import org.typelevel.ci.CIString
 
 import scala.collection.immutable.VectorMap
@@ -13,12 +15,18 @@ sealed abstract class Headers[A]:
   final def product[B](headers: Headers[B]): Headers[(A, B)] = Headers.Product(this, headers)
 
   final transparent inline def zip[B](headers: Headers[B]): Headers[?] = inline (this, headers) match
-    case (left: Headers[Void], right)  => ???
-    case (left, right: Headers[Void])  => ???
-    case (left: Headers[Tuple], right) => ???
-    case (left, right)                 => ???
+    case (left: Headers[Void], right) => left.product(right).imap[B] { case (_, b) => b }(b => (Void, b))
+    case (left, right: Headers[Void]) => left.product(right).imap[A] { case (a, _) => a }(a => (a, Void))
+    case (left: Headers[Tuple], right) =>
+      left.product(right).imap[Tuple.Append[A, B]] { case (a, b) => a :* b }(ab => (ab.init, ab.last.asInstanceOf[B]))
+    case (left, right) => left.product(right)
 
   final transparent inline def :*[B](header: Header[B]): Headers[?] = zip(header.toHeaders)
+
+  final def ivalidate[B: Encoder, C](validation: Validation[B, A, A, C])(g: C => A): Headers[C] =
+    Headers.Validate(this, validation, g)
+
+  final def imap[B](f: A => B)(g: B => A): Headers[B] = ivalidate(Validation.lift(f))(g)
 
   final def decode(headers: VectorMap[CIString, OpenApi.Primitive]): Validated[Violations, A] =
     decodeWithRemainders(headers).map(_._2)
@@ -51,5 +59,24 @@ object Headers:
         case Validated.Valid((remainders, a)) => right.decodeWithRemainders(remainders).map(_.tupleLeft(a))
         case Validated.Invalid(violations)    => right.decode(headers).fold(violations merge _, _ => violations).invalid
     override def encode(ab: (A, B)): VectorMap[CIString, OpenApi.Primitive] = left.encode(ab._1) ++ right.encode(ab._2)
+
+  final private case class Validate[A, B: Encoder, C](
+      headers: Headers[A],
+      validation: Validation[B, A, A, C],
+      g: C => A
+  ) extends Headers[C]:
+    override def decodeWithRemainders(
+        values: VectorMap[CIString, OpenApi.Primitive]
+    ): Validated[Violations, (VectorMap[CIString, OpenApi.Primitive], C)] = headers
+      .decodeWithRemainders(values)
+      .andThen(
+        _.traverse(
+          applyValidation(
+            validation,
+            a => OpenApi.fromMap(headers.encode(a).map { case (key, value) => (key.toString, value) })
+          )
+        )
+      )
+    override def encode(c: C): VectorMap[CIString, OpenApi.Primitive] = headers.encode(g(c))
 
   def apply[A](header: Header[A]): Headers[A] = Root(header)
--- modules/schema/src/main/scala/io/taig/openapi/schema/root.scala
+++ modules/schema/src/main/scala/io/taig/openapi/schema/root.scala
@@ -9,7 +9,7 @@ import io.taig.openapi.validation.{Constraint, Validation}
 private def typeViolations(tpe: String, actual: OpenApi): Violations =
   Violations.rootNec(Constraint.tpe(s"OpenApi.$tpe").map(OpenApi.fromString).toViolation(actual))
 
-private def applyValidation[A: Encoder, B, C](validation: Validation[A, B, B, C], encode: B => OpenApi)(
+private[openapi] def applyValidation[A: Encoder, B, C](validation: Validation[A, B, B, C], encode: B => OpenApi)(
     b: B
 ): Validated[Violations, C] = validation
   .run(b)
