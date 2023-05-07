90860500614b9e426e13bde27c57b6f62178d9e7
Niklas Klein <mail@taig.io>
2023-05-07 23:19:56 +0200

    Result = easy

--- modules/http/src/main/scala/io/taig/openapi/http/Response.scala
+++ modules/http/src/main/scala/io/taig/openapi/http/Response.scala
@@ -1,12 +1,14 @@
 package io.taig.openapi.http
 
-import cats.data.Chain
 import io.taig.openapi.{Encoder, OpenApi}
 import org.typelevel.ci.CIString
 
-final case class Response[F[_]](code: Code, headers: Chain[(CIString, String)], body: Response.Body):
-  def modifyHeaders(f: Chain[(CIString, String)] => Chain[(CIString, String)]): Response[F] = copy(headers = f(headers))
-  def withHeaders(headers: Chain[(CIString, String)]): Response[F] = modifyHeaders(_ => headers)
+import scala.collection.immutable.VectorMap
+
+final case class Response(code: Code, headers: VectorMap[CIString, String], body: Response.Body):
+  def modifyHeaders(f: VectorMap[CIString, String] => VectorMap[CIString, String]): Response =
+    copy(headers = f(headers))
+  def withHeaders(headers: VectorMap[CIString, String]): Response = modifyHeaders(_ => headers)
 
 object Response:
   enum Body:
---
+++ modules/http/src/main/scala/io/taig/openapi/http/Result.scala
@@ -0,0 +1,43 @@
+package io.taig.openapi.http
+
+import cats.data.Validated
+import cats.syntax.all.*
+import io.taig.openapi.{History, OpenApi}
+import io.taig.openapi.schema.{Violations, Void}
+import io.taig.openapi.validation.Constraint
+
+sealed abstract class Result[A]:
+  def code: Code
+  def headers: Headers[?]
+  final def imap[B](f: A => B)(g: B => A): Result[B] = Result.Modify(this, f, g)
+  final def decode(response: Response): Validated[Violations, A] = decodeWithRemainders(response).map(_._2)
+  def decodeWithRemainders(response: Response): Validated[Violations, (Response, A)]
+  def encode(a: A): Response
+
+object Result:
+  final private case class Root[A, B](code: Code, headers: Headers[A], body: Output.Body[B]) extends Result[(A, B)]:
+    override def decodeWithRemainders(response: Response): Validated[Violations, (Response, (A, B))] =
+      if response.code =!= code
+      then
+        Violations
+          .oneNec(
+            History.Root / "code",
+            Constraint.text.equal(OpenApi.fromInt(code.toInt)).toViolation(OpenApi.fromInt(response.code.toInt))
+          )
+          .invalid
+      else
+        (headers.decodeWithRemainders(response.headers), body.decode(response.body))
+          .mapN { case ((remainders, a), b) => (response.withHeaders(remainders), (a, b)) }
+    override def encode(ab: (A, B)): Response = Response(code, headers.encode(ab._1), body.encode(ab._2))
+
+  final private case class Modify[A, B](result: Result[A], f: A => B, g: B => A) extends Result[B]:
+    export result.{code, headers}
+    override def decodeWithRemainders(response: Response): Validated[Violations, (Response, B)] =
+      result.decodeWithRemainders(response).map(_.map(f))
+    override def encode(b: B): Response = result.encode(g(b))
+
+  transparent inline def apply[A, B](code: Code, headers: Headers[A], body: Output.Body[B]): Result[?] =
+    inline (headers, body) match
+      case (headers: Headers[Void], body)     => Root(code, headers, body).imap { case (_, b) => b }(b => (Void, b))
+      case (headers, body: Output.Body[Void]) => Root(code, headers, body).imap { case (a, _) => a }(a => (a, Void))
+      case _                                  => Root(code, headers, body)
