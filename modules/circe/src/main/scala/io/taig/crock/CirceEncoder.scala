8f6f81494e89df9f9adef396d3c92499d626518f
Niklas Klein <mail@taig.io>
2023-08-01 17:43:38 +0200

    Bring back field data structure

--- modules/circe/src/main/scala/io/taig/crock/CirceEncoder.scala
+++ modules/circe/src/main/scala/io/taig/crock/CirceEncoder.scala
@@ -8,9 +8,10 @@ import scala.annotation.tailrec
 object CirceEncoder:
   val schema: Encoder[Schema, Json] = new Encoder[Schema, Json]:
     override def encode[B](schema: Schema[B], b: B): Json = schema match
-      case schema: Primitive[B]  => primitive.encode(schema, b)
-      case schema: Collection[B] => collection.encode(schema, b)
-      case _                     => ???
+      case schema: Primitive[B]   => primitive.encode(schema, b)
+      case schema: Collection[B]  => collection.encode(schema, b)
+      case schema: Enumeration[B] => enumeration.encode(schema, b)
+      case _                      => ???
 
   val primitive: Encoder[Primitive, Json] = new Encoder[Primitive, Json]:
     def encode[B](tpe: Type[B], b: B): Json = tpe match
@@ -33,3 +34,8 @@ object CirceEncoder:
     override def encode[B](schema: Collection[B], b: B): Json = schema match
       case Collection.Root(of, _)            => Json.fromValues(b.map(CirceEncoder.schema.encode(of.value, _)))
       case Collection.Validate(schema, _, g) => encode(schema, g(b))
+
+  val enumeration: Encoder[Enumeration, Json] = new Encoder[Enumeration, Json]:
+    override def encode[B](schema: Enumeration[B], b: B): Json = schema match
+      case Enumeration.Root(mapping, schema, _)    => CirceEncoder.schema.encode(schema.value, mapping.inj(b))
+      case Enumeration.Validate(enumeration, _, g) => encode(enumeration, g(b))
--- modules/schema/src/main/scala/io/taig/crock/schema/Field.scala
+++ modules/schema/src/main/scala/io/taig/crock/schema/Field.scala
@@ -1,63 +1,83 @@
-//package io.taig.crock.schema
-//
-//import cats.data.Validated
-//import cats.syntax.all.*
-//import cats.{Eq, Eval}
-//import io.taig.crock.validation.Constraint
-//import io.taig.crock.{History, OpenApi}
-//
-//sealed abstract class Field[A, B]:
-//  self =>
-//
-//  def default: Option[B]
-//  def modifyDefault(f: Option[B] => Option[B]): Field[A, B]
-//
-//  def key: Eval[Schema.Value[A]]
-//
-//  def name: A
-//  final def renderName: String = key.value.render(name)
-//
-//  def nulls: Field.Null
-//  def modifyNulls(f: Field.Null => Field.Null): Field[A, B]
-//  final def withNulls(value: Field.Null): Field[A, B] = modifyNulls(_ => value)
-//  final def hideNulls: Field[A, B] = withNulls(Field.Null.Hide)
-//  final def showNulls: Field[A, B] = withNulls(Field.Null.Show)
-//  final def inheritNulls: Field[A, B] = withNulls(Field.Null.Inherit)
-//
-//  def schema: Eval[Schema[?]]
-//
-//  final def optional: Field[A, Option[B]] = Field.Optional(this)
-//
+package io.taig.crock.schema
+
+import cats.syntax.all.*
+import cats.{Eq, Eval}
+
+import scala.annotation.targetName
+import monocle.syntax.all.*
+
+sealed abstract class Field[A, B]:
+  self =>
+
+  trait Property[C]:
+    def value: C
+    def modify(f: C => C): Field[A, B]
+    final def apply(value: C): Field[A, B] = modify(_ => value)
+
+  object Property:
+    trait Optional[C] extends Property[Option[C]]:
+      @targetName("as")
+      def apply(value: C): Field[A, B] = apply(Some(value))
+      def clear: Field[A, B] = apply(None)
+
+    object Optional:
+      def apply[C](c: Option[C], g: (Option[C] => Option[C]) => Field[A, B]): Property.Optional[C] = new Optional[C]:
+        override def value: Option[C] = c
+        override def modify(f: Option[C] => Option[C]): Field[A, B] = g(f)
+
+    trait Nulls extends Property[Field.Null]:
+      def inherit: Field[A, B] = apply(Field.Null.Inherit)
+      def hide: Field[A, B] = apply(Field.Null.Hide)
+      def show: Field[A, B] = apply(Field.Null.Show)
+
+    object Nulls:
+      def apply(nulls: Field.Null, g: (Field.Null => Field.Null) => Field[A, B]): Property.Nulls = new Nulls:
+        override def value: Field.Null = nulls
+        override def modify(f: Field.Null => Field.Null): Field[A, B] = g(f)
+
+  def name: A
+  def key: Eval[Schema.Value[A]]
+
+  def default: Property.Optional[B]
+  def nulls: Property.Nulls
+
+  def schema: Eval[Schema[?]]
+
+  final def optional: Field[A, Option[B]] = Field.Optional(this)
+
 //  final transparent inline infix def zip[C](field: Field[A, C]): Product[A, ?] = toProduct zip field.toProduct
 //  final transparent inline infix def :*[C](field: Field[A, C]): Product[A, ?] = toProduct :* field
-//
+
 //  final def toProduct: Product[A, B] = Product(this)
 //  final def to[C](using Evidence.Product.Aux[C, B]): Product[A, C] = toProduct.to[C]
-//
-//  def decode(crock: OpenApi.Object): Validated[Violations, (OpenApi.Object, B)]
-//
-//  def encode(b: B, parent: Product.Nulls): OpenApi.Object
-//
-//object Field:
-//  enum Null:
-//    case Hide
-//    case Inherit
-//    case Show
-//
-//  object Null:
-//    val Default: Field.Null = Null.Inherit
-//
-//    given Eq[Null] = Eq.fromUniversalEquals
-//
-//  final private case class Required[A, B](
-//      default: Option[B],
-//      key: Eval[Schema.Value[A]],
-//      name: A,
-//      nulls: Field.Null,
-//      schema: Eval[Schema[B]]
-//  ) extends Field[A, B]:
-//    override def modifyDefault(f: Option[B] => Option[B]): Field[A, B] = copy(default = f(default))
-//    override def modifyNulls(f: Field.Null => Field.Null): Field[A, B] = copy(nulls = f(nulls))
+
+object Field:
+  enum Null:
+    case Hide
+    case Inherit
+    case Show
+
+  object Null:
+    val Default: Field.Null = Null.Inherit
+
+    given Eq[Null] = Eq.fromUniversalEquals
+
+  final case class Properties[+A](default: Option[A], nulls: Field.Null)
+
+  object Properties:
+    val Empty: Field.Properties[Nothing] = Properties(None, Field.Null.Default)
+
+  final private case class Required[A, B](
+      key: Eval[Schema.Value[A]],
+      name: A,
+      schema: Eval[Schema[B]],
+      properties: Field.Properties[B]
+  ) extends Field[A, B]:
+    override def default: Property.Optional[B] =
+      Property.Optional(properties.default, this.focus(_.properties.default).modify)
+    override def nulls: Property.Nulls =
+      Property.Nulls(properties.nulls, this.focus(_.properties.nulls).modify)
+
 //    override def decode(crock: OpenApi.Object): Validated[Violations, (OpenApi.Object, B)] =
 //      val name = renderName
 //      crock.get(name) match
@@ -72,12 +92,14 @@
 //    override def encode(b: B, parent: Product.Nulls): OpenApi.Object =
 //      OpenApi.Object.one(renderName, schema.value.encode(b))
 //
-//  final private case class Optional[A, B](field: Field[A, B]) extends Field[A, Option[B]]:
-//    export field.{key, name, nulls, schema}
-//    override def default: Option[Option[B]] = field.default.map(_.some)
-//    override def modifyDefault(f: Option[Option[B]] => Option[Option[B]]): Field[A, Option[B]] =
-//      copy(field.modifyDefault(value => f(value.map(_.some)).flatten))
-//    override def modifyNulls(f: Null => Null): Field[A, Option[B]] = copy(field.modifyNulls(f))
+  final private case class Optional[A, B](field: Field[A, B]) extends Field[A, Option[B]]:
+    export field.{key, name, schema}
+    override def default: Property.Optional[Option[B]] = Property.Optional(
+      field.default.value.map(_.some),
+      f => copy(field = field.default.modify(value => f(value.map(_.some)).flatten))
+    )
+    override def nulls: Property.Nulls = Property.Nulls(field.nulls.value, f => copy(field = field.nulls.modify(f)))
+
 //    override def decode(crock: OpenApi.Object): Validated[Violations, (OpenApi.Object, Option[B])] =
 //      if crock.contains(renderName)
 //      then field.decode(crock).map(_.map(_.some))
@@ -90,6 +112,6 @@
 //          case _                                                               => false
 //
 //        if dropNull then OpenApi.Object.Empty else OpenApi.Object.one(field.renderName, OpenApi.Null)
-//
-//  def apply[A, B](name: A, key: Eval[Schema.Value[A]], schema: Eval[Schema[B]]): Field[A, B] =
-//    Required[A, B](none, key, name, Null.Default, schema)
+
+  def apply[A, B](name: A, key: Eval[Schema.Value[A]], schema: Eval[Schema[B]]): Field[A, B] =
+    Required[A, B](key, name, schema, Properties.Empty)
