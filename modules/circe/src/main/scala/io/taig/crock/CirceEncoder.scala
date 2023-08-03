edb17339f10527e121913e2c6ca4d91429bf5a47
Niklas Klein <mail@taig.io>
2023-08-03 12:10:18 +0200

    CirceEncoder for Dictionary

--- modules/circe/src/main/scala/io/taig/crock/CirceEncoder.scala
+++ modules/circe/src/main/scala/io/taig/crock/CirceEncoder.scala
@@ -10,11 +10,12 @@ import scala.collection.mutable.ListBuffer
 
 object CirceEncoder:
   val schema: Encoder[Schema, Json] = new Encoder:
-    override def encode[A](schema: Schema[A], b: A): Json = schema match
-      case schema: Schema.Value[A] => value.encode(schema, b)
-      case schema: Collection[A]   => collection.encode(schema, b)
-      case schema: Record[A]       => record.encode(schema, b).fold(Json.Null)(Json.fromJsonObject)
-      case schema: Product[A]      => product.encode(schema, b).fold(Json.Null)(Json.fromValues)
+    override def encode[A](schema: Schema[A], a: A): Json = schema match
+      case schema: Schema.Value[A] => value.encode(schema, a)
+      case schema: Collection[A]   => collection.encode(schema, a)
+      case schema: Dictionary[A]   => dictionary.encode(schema, a).fold(Json.Null)(Json.fromJsonObject)
+      case schema: Record[A]       => record.encode(schema, a).fold(Json.Null)(Json.fromJsonObject)
+      case schema: Product[A]      => product.encode(schema, a).fold(Json.Null)(Json.fromValues)
 
   val value: Encoder[Schema.Value, Json] = new Encoder:
     override def encode[A](schema: Schema.Value[A], b: A): Json = schema match
@@ -22,15 +23,15 @@ object CirceEncoder:
       case schema: Enumeration[A] => enumeration.encode(schema, b)
 
   val primitive: Encoder[Primitive, Json] = new Encoder:
-    def encode[A](tpe: Type[A], b: A): Json = tpe match
-      case Type.BigDecimal => Json.fromBigDecimal(b)
-      case Type.BigInt     => Json.fromBigInt(b)
-      case Type.Boolean    => Json.fromBoolean(b)
-      case Type.Double     => Json.fromDoubleOrString(b)
-      case Type.Float      => Json.fromFloatOrString(b)
-      case Type.Int        => Json.fromInt(b)
-      case Type.Long       => Json.fromLong(b)
-      case Type.String     => Json.fromString(b)
+    def encode[A](tpe: Type[A], a: A): Json = tpe match
+      case Type.BigDecimal => Json.fromBigDecimal(a)
+      case Type.BigInt     => Json.fromBigInt(a)
+      case Type.Boolean    => Json.fromBoolean(a)
+      case Type.Double     => Json.fromDoubleOrString(a)
+      case Type.Float      => Json.fromFloatOrString(a)
+      case Type.Int        => Json.fromInt(a)
+      case Type.Long       => Json.fromLong(a)
+      case Type.String     => Json.fromString(a)
 
     override def encode[A](primitive: Primitive[A], b: A): Json = primitive match
       case Primitive.Root(_, tpe)         => encode(tpe, b)
@@ -38,16 +39,26 @@ object CirceEncoder:
       case Primitive.Optional(self)       => b.fold(Json.Null)(encode(self, _))
 
   val collection: Encoder[Collection, Json] = new Encoder:
-    override def encode[A](collection: Collection[A], b: A): Json = collection match
-      case Collection.Root(of, _)                   => Json.fromValues(b.map(CirceEncoder.schema.encode(of.value, _)))
-      case collection: Collection.Validate[?, ?, ?] => encode(collection.self, collection.g(b))
-      case collection: Collection.Optional[?, ?]    => b.fold(Json.Null)(encode(collection.self, _))
+    override def encode[A](collection: Collection[A], a: A): Json = collection match
+      case Collection.Root(of, _)                   => Json.fromValues(a.map(CirceEncoder.schema.encode(of.value, _)))
+      case collection: Collection.Validate[?, ?, ?] => encode(collection.self, collection.g(a))
+      case collection: Collection.Optional[?, ?]    => a.fold(Json.Null)(encode(collection.self, _))
+
+  val dictionary: Encoder[Dictionary, Option[JsonObject]] = new Encoder:
+    override def encode[A](dictionary: Dictionary[A], a: A): Option[JsonObject] = dictionary match
+      case Dictionary.Root(key, schema, _) =>
+        val values = a.map { case (k, v) =>
+          (StringEncoder.value.encode(key.value, k).orEmpty, CirceEncoder.schema.encode(schema.value, v))
+        }
+        JsonObject.fromIterable(values).some
+      case Dictionary.Validate(self, _, g) => encode(self, g(a))
+      case Dictionary.Optional(self)       => a.flatMap(encode(self, _))
 
   val enumeration: Encoder[Enumeration, Json] = new Encoder:
-    override def encode[A](enumeration: Enumeration[A], b: A): Json = enumeration match
-      case Enumeration.Root(mapping, schema, _) => CirceEncoder.schema.encode(schema.value, mapping.inj(b))
-      case Enumeration.Validate(self, _, g)     => encode(self, g(b))
-      case Enumeration.Optional(self)           => b.fold(Json.Null)(encode(self, _))
+    override def encode[A](enumeration: Enumeration[A], a: A): Json = enumeration match
+      case Enumeration.Root(mapping, schema, _) => CirceEncoder.schema.encode(schema.value, mapping.inj(a))
+      case Enumeration.Validate(self, _, g)     => encode(self, g(a))
+      case Enumeration.Optional(self)           => a.fold(Json.Null)(encode(self, _))
 
   val record: Encoder[Record, Option[JsonObject]] = new Encoder:
     override def encode[A](record: Record[A], a: A): Option[JsonObject] =
