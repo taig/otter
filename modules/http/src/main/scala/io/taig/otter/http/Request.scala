886986c4813f3207b3c641ade7754bac713dadf4
Niklas Klein <mail@taig.io>
2023-08-12 09:38:14 +0200

    Body starting to look good

--- modules/http/src/main/scala/io/taig/otter/http/Request.scala
+++ modules/http/src/main/scala/io/taig/otter/http/Request.scala
@@ -1,75 +1,94 @@
 package io.taig.otter.http
 
-import cats.data.Validated
+import cats.data.{Chain, Validated}
 import cats.syntax.all.*
+import io.taig.otter.http.Http.{Payload, Request}
+import io.taig.otter.http.Http.Request.Body
 import io.taig.otter.schema.Violations
-import io.taig.otter.validation.Validation
+import io.taig.otter.validation.Violation
 
 sealed abstract class Request[A]:
+  self =>
   def method: Method
   def url: Url[?]
   def headers: Headers[?]
   def body: Request.Body[?]
-  final def imap[B](f: A => B)(g: B => A): Request[B] = ???
+
+  final def andThen[B](f: A => Validated[Violations, B])(g: B => A): Request[B] = new Request[B]:
+    export self.{body, headers, method, url}
+    override def decode(request: Http.Request): Validated[Violations, B] = self.decode(request).andThen(f)
+    override def encode(b: B): Http.Request = self.encode(g(b))
+
+  final def imap[B](f: A => B)(g: B => A): Request[B] = andThen(f(_).valid)(g)
+
+  def decode(request: Http.Request): Validated[Violations, A]
+  def encode(a: A): Http.Request
 
 object Request:
   sealed abstract class Body[A]:
     self =>
     type Self[a] <: Body[a] { type Self[a] = self.Self[a] }
-    def withHeaders: Self[(Http.Headers, A)]
-    def andThen[B](f: A => Validated[Violations, B])(g: B => A): Self[B]
-    def ivalidate[B](validation: Validation[A, B])(g: B => A): Self[B]
-    final def validate(validation: Validation[A, Unit]): Self[A] = ???
-    final def imap[B](f: A => B)(g: B => A): Self[B] = ???
 
-//  object Body:
-//    sealed abstract class Singlepart[A] extends Request.Body[A]:
-//      self =>
-//      override type Self[a] <: Request.Body.Singlepart[a] { type Self[a] = self.Self[a] }
-//
-//    object Singlepart:
-//      sealed abstract class Strict[A] extends Request.Body.Singlepart[A]:
-//        final override type Self[a] = Request.Body.Singlepart.Strict[a]
-//        final override def withHeaders: Strict[(Http.Headers, A)] = Strict.WithHeaders(this)
-//        final override def andThen[B](f: A => Validated[Violations, B])(g: B => A): Strict[B] =
-//          Strict.AndThen(this, f, g)
-//        final override def ivalidate[B](validation: Validation[A, B])(g: B => A): Request.Body.Singlepart.Strict[B] =
-//          Strict.Validate(this, validation, g)
-//
-//      object Strict:
-//        private[otter] case object Bytes extends Request.Body.Singlepart.Strict[Array[Byte]]
-//
-//        final private[otter] case class WithHeaders[A](self: Request.Body.Singlepart.Strict[A])
-//            extends Request.Body.Singlepart.Strict[(Http.Headers, A)]
-//
-//        final private[otter] case class AndThen[A, B](
-//            self: Request.Body.Singlepart.Strict[A],
-//            f: A => Validated[Violations, B],
-//            g: B => A
-//        ) extends Request.Body.Singlepart.Strict[B]
-//
-//        final private[otter] case class Validate[A, B](
-//            self: Request.Body.Singlepart.Strict[A],
-//            validation: Validation[A, B],
-//            g: B => A
-//        ) extends Request.Body.Singlepart.Strict[B]
-//
-//        val Empty: Request.Body.Singlepart.Strict[Unit] = Bytes.imap(_ => ())(_ => Array.emptyByteArray)
-//
-//      sealed abstract class Streaming[A] extends Request.Body.Singlepart[A]:
-//        final override type Self[a] = Request.Body.Singlepart.Streaming[a]
-//        final override def withHeaders: Streaming[(Http.Headers, A)] = ???
-//        final override def andThen[B](f: A => Validated[Violations, B])(g: B => A): Streaming[B] = ???
-//        final override def ivalidate[B](validation: Validation[A, B])(g: B => A): Request.Body.Singlepart.Streaming[B] =
-//          Streaming.Validate(this, validation, g)
-//
-//      object Streaming:
-//        private[otter] case object Bytes extends Request.Body.Singlepart.Streaming[Stream[Byte]]
-//
-//        final private[otter] case class Validate[A, B](
-//            self: Request.Body.Singlepart.Streaming[A],
-//            validation: Validation[A, B],
-//            g: B => A
-//        ) extends Request.Body.Singlepart.Streaming[B]
-//
-//        val Empty: Request.Body.Singlepart.Streaming[Unit] = Bytes.imap(_ => ())(_ => Stream.Empty)
+    def andThen[B](f: A => Validated[Violations, B])(g: B => A): Self[B]
+    final def imap[B](f: A => B)(g: B => A): Self[B] = andThen(f(_).valid)(g)
+    def zip[B](headers: Headers[B]): Self[(A, B)]
+
+    def decode(headers: Http.Headers, body: Http.Request.Body): Validated[Violations, A]
+    def encode(a: A): (Http.Headers, Http.Request.Body)
+
+  object Body:
+    sealed abstract class Singlepart[A] extends Request.Body[A]:
+      self =>
+
+      override type Self[a] <: Request.Body.Singlepart[a] { type Self[a] = self.Self[a] }
+
+      override def decode(headers: Http.Headers, body: Http.Request.Body): Validated[Violations, A] = body match
+        case Http.Request.Body.Singlepart(payload) => decode(headers, payload)
+      def decode(headers: Http.Headers, body: Http.Payload): Validated[Violations, A]
+      override def encode(a: A): (Http.Headers, Http.Request.Body.Singlepart)
+
+    object Singlepart:
+      sealed abstract class Strict[A] extends Request.Body.Singlepart[A]:
+        self =>
+        final override type Self[a] = Request.Body.Singlepart.Strict[a]
+
+        final override def andThen[B](f: A => Validated[Violations, B])(g: B => A): Request.Body.Singlepart.Strict[B] =
+          new Strict[B]:
+            override def decodeWithRemainders(
+                remainders: Http.Headers,
+                payload: Array[Byte]
+            ): Validated[Violations, (Http.Headers, B)] =
+              self.decodeWithRemainders(remainders, payload).andThen(_.traverse(f))
+            override def encode(b: B): (Http.Headers, Http.Request.Body.Singlepart) = self.encode(g(b))
+
+        final override def zip[B](headers: Headers[B]): Request.Body.Singlepart.Strict[(A, B)] = new Strict[(A, B)]:
+          override def decodeWithRemainders(
+              remainders: Http.Headers,
+              payload: Array[Byte]
+          ): Validated[Violations, (Http.Headers, (A, B))] =
+            self.decodeWithRemainders(remainders, payload).andThen { case (remainders, a) =>
+              headers.decodeWithRemainders(remainders).map(_.tupleLeft(a))
+            }
+
+          override def encode(ab: (A, B)): (Http.Headers, Http.Request.Body.Singlepart) =
+            self.encode(ab._1).leftMap(_ ++ headers.encode(ab._2))
+
+        final override def decode(headers: Http.Headers, payload: Http.Payload): Validated[Violations, A] =
+          payload match
+            case Payload.Strict(data) => decode(headers, data)
+            case Payload.Streaming(_) => Violations.rootNec(Violation.tpe("strict", "streaming")).invalid
+        def decode(headers: Http.Headers, payload: Array[Byte]): Validated[Violations, A] =
+          decodeWithRemainders(headers, payload).map(_._2)
+        def decodeWithRemainders(
+            remainders: Http.Headers,
+            payload: Array[Byte]
+        ): Validated[Violations, (Http.Headers, A)]
+
+      object Strict:
+        val Root: Request.Body.Singlepart.Strict[Array[Byte]] = new Strict[Array[Byte]]:
+          override def encode(a: Array[Byte]): (Http.Headers, Http.Request.Body.Singlepart) =
+            (Chain.empty, Http.Request.Body.Singlepart(Http.Payload.Strict(a)))
+          override def decodeWithRemainders(
+              remainders: Http.Headers,
+              payload: Array[Byte]
+          ): Validated[Violations, (Http.Headers, Array[Byte])] = (remainders, payload).valid
