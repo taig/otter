b2afcf04fe4ec2593424c939c349097464b216c8
Niklas Klein <mail@taig.io>
2023-09-23 11:46:46 +0200

    Successfully dealing with json (:

--- modules/circe/src/main/scala/io/taig/otter/circe/syntax.scala
+++ modules/circe/src/main/scala/io/taig/otter/circe/syntax.scala
@@ -3,8 +3,11 @@ package io.taig.otter.circe
 import io.circe.Json
 import io.taig.otter.Schema
 import io.taig.otter.http.Request
+import io.taig.otter.schemas.*
 
 object syntax:
+  val json: Schema.Dynamic[Json] = dynamic.any.imap(fromData)(toData)
+
   object body:
     val json: Request.Body.Singlepart.Strict[Json] = ???
     def json[A](schema: Schema[A]): Request.Body.Singlepart.Strict[A] = ???
--- modules/sample/src/main/scala/io/taig/otter/sample/schemas/book.scala
+++ modules/sample/src/main/scala/io/taig/otter/sample/schemas/book.scala
@@ -6,6 +6,7 @@ import io.taig.otter.sample.Book
 import io.taig.otter.schemas.*
 import io.taig.otter.*
 import io.taig.otter.validation.validations.*
+import io.taig.otter.circe.syntax.*
 
 import scala.Tuple.Append
 import scala.collection.immutable.SortedSet
@@ -21,11 +22,9 @@ object book:
     case Book.Genre.Romance   => "romance"
     case Book.Genre.Thriller  => "thriller"
 
-  val metadata: Schema.Dynamic[Json] = ???
-
   val main: Schema.Record[Book] = (
     field("isbn", isbn) :*
       field("title", title) :*
       field("genres", collection.sortedSet(genre)) :*
-      field("metadata", metadata)
+      field("metadata", json)
   ).to[Book]
