e746633c7f5acf0ba4362ae439d43ea56e533205
Niklas Klein <mail@taig.io>
2023-09-29 19:58:21 +0200

    Looking into Result decoding

--- modules/http/src/main/scala/io/taig/otter/http/Response.scala
+++ modules/http/src/main/scala/io/taig/otter/http/Response.scala
@@ -8,7 +8,7 @@ import io.taig.otter.validation.{History, Violation, Violations}
 final case class Response[A](results: Results[A], violations: Result[Violations]):
   def modifyResults[T](f: Results[A] => Results[T]): Response[T] = copy(results = f(results))
 
-  def decode(response: Http.Response): Validated[Violations, A] = ???
+  def decode(response: Http.Response): Validated[Violations, A] = results.decode(response)
   def encode(a: Validated[Violations, A]): Http.Response = a.fold(violations.encode, results.encode)
 
 object Response:
--- modules/http/src/main/scala/io/taig/otter/http/Results.scala
+++ modules/http/src/main/scala/io/taig/otter/http/Results.scala
@@ -11,12 +11,14 @@ sealed abstract class Results[A]:
 
   final def imap[B](f: A => B)(g: B => A): Results[B] = new Results[B]:
     export self.toNonEmptyChain
-    override def decode(response: Http.Response): Validated[Violations, B] = self.decode(response).map(f)
+    override def decodeOption(response: Http.Response): Validated[Violations, Option[B]] =
+      self.decodeOption(response).map(_.map(f))
     override def encode(b: B): Http.Response = self.encode(g(b))
 
   final def orElse[B](results: Results[B]): Results[A + B] = new Results[A + B]:
     override def toNonEmptyChain: NonEmptyChain[Result[?]] = self.toNonEmptyChain.combine(results.toNonEmptyChain)
-    override def decode(response: Http.Response): Validated[Violations, A + B] = ???
+    override def decodeOption(response: Http.Response): Validated[Violations, Option[A + B]] = ???
+    // self.decode(response).map(_.asLeft).findValid(results.decode(response).map(_.asRight))
     override def encode(ab: A + B): Http.Response = ab match
       case Left(a)  => self.encode(a)
       case Right(b) => results.encode(b)
@@ -26,12 +28,13 @@ sealed abstract class Results[A]:
 
   final def to[B](using evidence: Evidence.Coproduct.Aux[B, A]): Results[B] = imap(evidence.from)(evidence.to)
 
-  def decode(response: Http.Response): Validated[Violations, A]
+  final def decode(response: Http.Response): Validated[Violations, A] =
+    decodeOption(response).andThen(_.toValid(???))
+  protected def decodeOption(response: Http.Response): Validated[Violations, Option[A]]
   def encode(a: A): Http.Response
 
 object Results:
   def apply[A](result: Result[A]): Results[A] = new Results[A]:
     override def toNonEmptyChain: NonEmptyChain[Result[?]] = NonEmptyChain.one(result)
-    override def decode(response: Http.Response): Validated[Violations, A] =
-      result.decode(response).andThen(_.toValid(???))
+    override def decodeOption(response: Http.Response): Validated[Violations, Option[A]] = result.decode(response)
     override def encode(a: A): Http.Response = result.encode(a)
