b565fe4627b1faaee76292964deeec9209014b79
Niklas Klein <mail@taig.io>
2023-10-02 00:14:04 +0200

    This is the way

--- modules/core/src/main/scala/io/taig/otter/Collection.scala
+++ modules/core/src/main/scala/io/taig/otter/Collection.scala
@@ -4,26 +4,25 @@ import cats.data.{Chain, Validated}
 import cats.syntax.all.*
 import io.taig.otter.validation.{Constraint, Validation, Violation, Violations}
 
-sealed abstract class Collection[A](description: Option[String]) extends Schema[A](description):
+sealed abstract class Collection[A](description: Option[String], val schema: Schema[?]) extends Schema[A](description):
   self =>
   override type Self[a] = Collection.Of[Of, a]
-  type Of[a] <: Schema[a]
-  type T
+  type Of <: Schema[?]
 
   final override def description(f: Option[String] => Option[String]): Collection.Of[Of, A] =
     Collection(this, description)
 
-  final override def optional: Collection.Of[Of, Option[A]] = new Collection[Option[A]](description):
-    export self.{constraints, Of, T}
+  final override def optional: Collection.Of[Of, Option[A]] = new Collection[Option[A]](description, schema):
+    export self.{constraints, Of}
     override def isOptional: Boolean = true
     override def decodeArray(data: Option[Data.Array]): Validated[Violations, Option[A]] =
       data.fold(none.valid)(_ => self.decodeArray(data).map(_.some))
     override def encodeArray(a: Option[A]): Option[Chain[Data]] = a.flatMap(self.encodeArray)
     override def parse(values: Option[Chain[Option[String]]])(using
-        Of[T] <:< Schema.Value[T]
+        Of <:< Schema.Value[?]
     ): Validated[Violations, Option[A]] =
       values.fold(none.valid)(_ => self.parse(values).map(_.some))
-    override def print(a: Option[A])(using Of[T] <:< Schema.Value[T]): Option[Chain[Option[String]]] =
+    override def print(a: Option[A])(using Of <:< Schema.Value[?]): Option[Chain[Option[String]]] =
       a.flatMap(self.print)
 
   final override def ivalidate[B](validation: Validation[A, B])(g: B => A): Collection.Of[Of, B] = ???
@@ -36,33 +35,33 @@ sealed abstract class Collection[A](description: Option[String]) extends Schema[
   final override def encode(a: A): Data = encodeArray(a).map(Data.Array.apply).getOrElse(Data.Null)
   def encodeArray(a: A): Option[Chain[Data]]
 
-  def parse(values: Option[Chain[Option[String]]])(using Of[T] <:< Schema.Value[T]): Validated[Violations, A]
-  def print(a: A)(using Of[T] <:< Schema.Value[T]): Option[Chain[Option[String]]]
+  def parse(values: Option[Chain[Option[String]]])(using Of <:< Schema.Value[?]): Validated[Violations, A]
+  def print(a: A)(using Of <:< Schema.Value[?]): Option[Chain[Option[String]]]
 
 object Collection:
-  type Of[F[a] <: Schema[a], A] = Collection[A] { type Of[a] <: F[a] }
+  type Of[A <: Schema[?], B] = Collection[B] { type Of <: A }
 
-  def apply[F[a] <: Schema[a], A](schema: Collection.Of[F, A], description: Option[String]): Collection.Of[F, A] =
-    new Collection[A](description) { export schema.* }
+  def apply[A <: Schema[?], B](of: Collection.Of[A, B], description: Option[String]): Collection.Of[A, B] =
+    new Collection[B](description, of.schema) { export of.* }
 
-  def apply[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F, Chain[A]] = new Collection[Chain[A]](None):
-    override type Of[a] = F[a]
-    override type T = A
+  def apply[F[a] <: Schema[a], A](of: F[A]): Collection.Of[F[A], Chain[A]] = new Collection[Chain[A]](None, of):
+    override type Of = F[A]
     override def constraints: Chain[Constraint] = Chain.empty
     override def isOptional: Boolean = false
     override def decodeArray(data: Option[Data.Array]): Validated[Violations, Chain[A]] = Validated
       .fromOption(data, Violations.rootNec(Violation.required))
       .andThen(_.values.zipWithIndex.traverse { case (data, index) =>
-        schema.decode(data).leftMap(_.modifyHistory(index /: _))
+        of.decode(data).leftMap(_.modifyHistory(index /: _))
       })
-    override def encodeArray(a: Chain[A]): Option[Chain[Data]] = a.map(schema.encode).some
+    override def encodeArray(a: Chain[A]): Option[Chain[Data]] = a.map(of.encode).some
 
     override def parse(
         values: Option[Chain[Option[String]]]
-    )(using F[A] <:< Schema.Value[A]): Validated[Violations, Chain[A]] = Validated
-      .fromOption(values, Violations.rootNec(Violation.required))
-      .andThen(_.zipWithIndex.traverse { case (value, index) =>
-        schema.parse(value).leftMap(_.modifyHistory(index /: _))
-      })
-    override def print(a: Chain[A])(using F[A] <:< Schema.Value[A]): Option[Chain[Option[String]]] =
-      a.map(schema.print).some
+    )(using F[A] <:< Schema.Value[?]): Validated[Violations, Chain[A]] =
+      Validated
+        .fromOption(values, Violations.rootNec(Violation.required))
+        .andThen(_.zipWithIndex.traverse { case (value, index) =>
+          of.asInstanceOf[Schema.Value[A]].parse(value).leftMap(_.modifyHistory(index /: _))
+        })
+    override def print(a: Chain[A])(using F[A] <:< Schema.Value[?]): Option[Chain[Option[String]]] =
+      Some(a.map(of.asInstanceOf[Schema.Value[A]].print))
--- modules/core/src/main/scala/io/taig/otter/schemas.scala
+++ modules/core/src/main/scala/io/taig/otter/schemas.scala
@@ -54,20 +54,20 @@ object schemas:
   def branch[A](name: Int, schema: Schema[A]): Branch[A] = branch(name, int, schema)
 
   object collection:
-    def chain[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F, Chain[A]] = Collection(schema)
-    def vector[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F, Vector[A]] =
+    def chain[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F[A], Chain[A]] = Collection(schema)
+    def vector[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F[A], Vector[A]] =
       chain(schema).imap(_.toVector)(Chain.fromSeq)
-    def list[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F, List[A]] =
+    def list[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F[A], List[A]] =
       chain(schema).imap(_.toList)(Chain.fromSeq)
-    def sortedSet[F[a] <: Schema[a], A: Ordering](schema: F[A]): Collection.Of[F, SortedSet[A]] =
+    def sortedSet[F[a] <: Schema[a], A: Ordering](schema: F[A]): Collection.Of[F[A], SortedSet[A]] =
       chain(schema).imap(values => SortedSet.from(values.iterator))(Chain.fromIterableOnce)
-    def nonEmptyChain[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F, NonEmptyChain[A]] =
+    def nonEmptyChain[F[a] <: Schema[a], A](schema: F[A]): Collection.Of[F[A], NonEmptyChain[A]] =
       val validation: Validation[Chain[A], NonEmptyChain[A]] =
         Validation(Constraint.MinItems(1))(NonEmptyChain.fromChain(_).toValidNec(Data.Number(0)))
       chain(schema).ivalidate(validation)(_.toChain)
     def sortedMap[F[a] <: Schema[a], A: Ordering, B](key: Schema[A], schema: Schema[B])(
         f: (Schema[A], Schema[B]) => F[(A, B)]
-    ): Collection.Of[F, SortedMap[A, B]] =
+    ): Collection.Of[F[(A, B)], SortedMap[A, B]] =
       chain(f(key, schema)).imap(values => SortedMap.from(values.iterator))(Chain.fromIterableOnce)
 
   object enumeration:
