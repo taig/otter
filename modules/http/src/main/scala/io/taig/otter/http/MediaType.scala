e7374ed6bbca7f731e96662bb6b6f46ae92243d1
Niklas Klein <mail@taig.io>
2023-10-03 14:10:43 +0200

    More advanced media types

--- modules/http/src/main/scala/io/taig/otter/http/MediaType.scala
+++ modules/http/src/main/scala/io/taig/otter/http/MediaType.scala
@@ -2,17 +2,49 @@ package io.taig.otter.http
 
 import cats.Eq
 
-opaque type MediaType = String
+final case class MediaType(tpe: MediaType.Type, subtype: MediaType.Subtype):
+  def print: String = s"$tpe/$subtype"
+  override def toString: String = print
 
 object MediaType:
-  extension (self: MediaType) def toString: String = self
-  def apply(value: String): MediaType = value
+  opaque type Type = String
 
-  given (using eq: Eq[String]): Eq[MediaType] = eq
+  object Type:
+    extension (self: MediaType.Type)
+      def /(subtype: MediaType.Subtype): MediaType = MediaType(self, subtype)
+      def toString: String = self
+
+    def apply(value: String): MediaType.Type = value
+
+    val * : MediaType.Type = "*"
+    val application: MediaType.Type = "application"
+    val text: MediaType.Type = "text"
+
+    given (using eq: Eq[String]): Eq[MediaType.Type] = eq
+
+  opaque type Subtype = String
+
+  object Subtype:
+    extension (self: MediaType.Subtype) def toString: String = self
+
+    def apply(value: String): MediaType.Subtype = value
+
+    val * : MediaType.Subtype = "*"
+    val json: MediaType.Subtype = "json"
+    val plain: MediaType.Subtype = "plain"
+    val octetStream: MediaType.Subtype = "octet-stream"
+
+    given (using eq: Eq[String]): Eq[MediaType.Subtype] = eq
+
+  def parse(value: String): Option[MediaType] = value.split('/') match
+    case Array(tpe, subtype) if tpe.nonEmpty && subtype.nonEmpty => Some(Type(tpe) / Subtype(subtype))
+    case _                                                       => None
 
   object application:
-    val json: MediaType = "application/json"
-    val octetStream: MediaType = "application/octet-stream"
+    def apply(subtype: MediaType.Subtype): MediaType = Type.application / subtype
+    val json: MediaType = application(Subtype.json)
+    val octetStream: MediaType = application(Subtype.octetStream)
 
   object text:
-    val plain: MediaType = "text/plain"
+    def apply(subtype: MediaType.Subtype): MediaType = Type.text / subtype
+    val plain: MediaType = text(Subtype.plain)
--- modules/http/src/main/scala/io/taig/otter/http/headers/ContentType.scala
+++ modules/http/src/main/scala/io/taig/otter/http/headers/ContentType.scala
@@ -5,17 +5,20 @@ import io.taig.otter.http.MediaType
 import io.taig.otter.validation.{validations, Validation}
 
 final case class ContentType(mediaType: MediaType, charset: Option[String]):
-  def print: String = (mediaType.toString :: charset.map(charset => s"charset=$charset").toList).mkString("; ")
+  def print: String = (mediaType.print :: charset.map(charset => s"charset=$charset").toList).mkString("; ")
 
 object ContentType:
   def parse(value: String): Option[ContentType] = value.split(";\\s*").toList match
     case mediaType :: tail =>
-      val charset = tail
-        .map(_.split("\\s*=\\s*", 2))
-        .collectFirst { case Array(name, value) if name.equalsIgnoreCase("charset") => value }
-        .map(charset => if charset.startsWith("\"") && charset.endsWith("\"") then charset.tail.init else charset)
+      MediaType
+        .parse(mediaType)
+        .map: mediaType =>
+          val charset = tail
+            .map(_.split("\\s*=\\s*", 2))
+            .collectFirst { case Array(name, value) if name.equalsIgnoreCase("charset") => value }
+            .map(charset => if charset.startsWith("\"") && charset.endsWith("\"") then charset.tail.init else charset)
 
-      ContentType(MediaType(mediaType), charset).some
+          ContentType(mediaType, charset)
     case Nil => none
 
   val validation: Validation[String, ContentType] = validations.parse("contentType")(parse)
