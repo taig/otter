27c429b552a147f77958630f78b3edfb35261626
Niklas Klein <mail@taig.io>
2024-01-23 23:46:22 +0100

    Fix enumeration decoding

--- modules/core/src/main/scala/io/taig/otter/Codec.scala
+++ modules/core/src/main/scala/io/taig/otter/Codec.scala
@@ -114,6 +114,7 @@ sealed abstract class Collection[A](val description: Option[String], val name: O
       export self.{codec, isOptional, Of}
       override def constraints: Chain[Constraint] = self.constraints ++ validation.constraints
       override def decodeArray(data: Option[Data.Array]): Validated[Violations, B] =
+        println("decode collection validate")
         self.decodeArray(data).andThen(validation(_).leftMap(Violations.root))
       override def encodeArray(b: B): Option[Chain[Data]] = self.encodeArray(g(b))
       override def parse(values: Option[Chain[String]])(using Of <:< Value[?]): Validated[Violations, B] =
@@ -297,9 +298,9 @@ sealed abstract class Dictionary[A](val description: Option[String], val name: O
       override def encodeObject(b: B): Option[Data.Object] = self.encodeObject(g(b))
 
   final override def decode(data: Option[Data.Value]): Validated[Violations, A] = data match
-    case Some(_: Data.Object) => decode(data.asInstanceOf[Option[Data.Object]])
+    case Some(_: Data.Object) => decodeObject(data.asInstanceOf[Option[Data.Object]])
     case Some(data)           => Violations.rootNec(Violation.tpe("object", data.name)).invalid
-    case None                 => decode(None)
+    case None                 => decodeObject(None)
   def decodeObject(data: Option[Data.Object]): Validated[Violations, A]
 
   final override def encode(a: A): Data = encodeObject(a).getOrElse(Data.Null)
@@ -508,11 +509,10 @@ object Primitive:
     def apply[A](of: Type[A]): Primitive.Required[A] = new Required[A](None, None, None):
       override def tpe: Type[?] = of
       override def constraints: Chain[Constraint] = Chain.empty
-      override def decode(data: Option[Data.Value]): Validated[Violations, A] =
-        data match
-          case Some(data: Data.Primitive) => of.decode(data)
-          case Some(data)                 => Violations.rootNec(Violation.tpe(of.name, actual = data.name)).invalid
-          case None                       => Violations.rootNec(Violation.required).invalid
+      override def decode(data: Option[Data.Value]): Validated[Violations, A] = data match
+        case Some(data: Data.Primitive) => of.decode(data)
+        case Some(data)                 => Violations.rootNec(Violation.tpe(of.name, actual = data.name)).invalid
+        case None                       => Violations.rootNec(Violation.required).invalid
       override def encode(a: A): Data.Primitive = of.encode(a)
       override def parse(value: String): Validated[Violations, A] = Validated.fromOption(
         of.parse(value),
