2d5d7418e90cc7ce6dbccc2816067358b4dd7748
Niklas Klein <mail@taig.io>
2024-04-05 14:26:20 +0200

    Back to basic data structure now let's go from here

--- modules/core/src/main/scala/io/taig/otter/Encoder.scala
+++ modules/core/src/main/scala/io/taig/otter/Encoder.scala
@@ -1,10 +1,10 @@
 package io.taig.otter
 
 abstract class Encoder[T]:
-  final def apply[A](schema: Schema[?, A], value: A): T = schema match
-    case schema: Primitive[?, ?] => apply(schema, value)
-    case schema: Product[?, ?]   => apply(schema, value)
+  final def apply[A](schema: Schema[A], value: A): T = schema match
+    case schema: Primitive[?] => apply(schema, value)
+    case schema: Product[?]   => apply(schema, value)
 
-  def apply[A](schema: Primitive[?, A], value: A): T
+  def apply[A](schema: Primitive[A], value: A): T
 
-  def apply[A](schema: Product[?, A], value: A): T
+  def apply[A](schema: Product[A], value: A): T
