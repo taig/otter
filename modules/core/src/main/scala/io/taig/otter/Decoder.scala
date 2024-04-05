8b3a6099a2a673b2d2ba9c1837b9927bfe060a2c
Niklas Klein <mail@taig.io>
2024-04-05 21:04:24 +0200

    Reapply "Back to basic data structure now let's go from here"
    This reverts commit f3733b21ca2f14e16682a8114b214d3cee80a675.

--- modules/core/src/main/scala/io/taig/otter/Decoder.scala
+++ modules/core/src/main/scala/io/taig/otter/Decoder.scala
@@ -4,10 +4,10 @@ import cats.data.Validated
 import io.taig.otter.validation.Violations
 
 abstract class Decoder[T]:
-  final def apply[A](schema: Schema[?, A], value: T): Validated[Violations, A] = schema match
-    case schema: Primitive[?, ?] => apply(schema, value)
-    case schema: Product[?, ?]   => apply(schema, value)
+  final def apply[A](schema: Schema[A], value: T): Validated[Violations, A] = schema match
+    case schema: Primitive[?] => apply(schema, value)
+    case schema: Product[?]   => apply(schema, value)
 
-  def apply[A](schema: Primitive[?, A], value: T): Validated[Violations, A]
+  def apply[A](schema: Primitive[A], value: T): Validated[Violations, A]
 
-  def apply[A](schema: Product[?, A], value: T): Validated[Violations, A]
+  def apply[A](schema: Product[A], value: T): Validated[Violations, A]
