c3e8c2d68118cd10d1f91460a8e28fc592387db9
Niklas Klein <mail@taig.io>
2024-05-29 08:47:11 +0200

    Save progress

--- modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonCollectionEncoder.scala
+++ modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonCollectionEncoder.scala
@@ -1,11 +1,18 @@
-// package io.taig.otter.json.circe
+package io.taig.otter.json.circe
 
-// import io.taig.otter as Base
-// import io.circe.Json
-// import cats.syntax.all.*
-// import cats.Id as Identity
+import io.taig.otter as Base
+import io.circe.Json
+import cats.syntax.all.*
+import cats.Id as Identity
 
-// object JsonCollectionEncoder:
-//   def apply[A](data: Base.Collection[Base.Knot.Writer[Identity], ?, A], a: A): Option[Vector[Json]] =
-//     data match
-//       case Base.Collection.Root(schema) => a.map(JsonEncoder(schema, _)).some
+object JsonCollectionEncoder:
+  def apply[A](
+      data: Base.Collection[
+        Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, ?, *], *], *],
+        ?,
+        ?,
+        A
+      ],
+      a: A
+  ): Vector[Json] = data match
+    case Base.Collection.Root(schema) => a.map(JsonEncoder.apply(schema, _)).some
--- modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonEncoder.scala
+++ modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonEncoder.scala
@@ -1,6 +1,7 @@
 package io.taig.otter.json.circe
 
 import io.circe.Json
+import cats.Id as Identity
 import io.taig.otter.Schema.*
 import io.taig.otter.Plain.*
 import io.taig.otter.Writer
@@ -11,16 +12,31 @@ import io.taig.otter.Isomorphic
 
 object JsonEncoder extends Encoder[Schema.Writer, Json]:
   override def apply[A](schema: Schema.Writer[A], a: A): Json = schema match
-    case Writer.Root(schema)           => apply(schema, a)
-    case Isomorphic.Root(schema)       => apply(schema, a)
+    case Writer.Root(schema)           => apply0(schema, a)
+    case Isomorphic.Root(schema)       => apply0(schema, a)
     case Writer.Modify(self, f)        => apply(self, f(a))
     case Isomorphic.Modify(self, _, f) => apply(self, f(a))
 
-  def apply[A](schema: Base.Schema[Base.Data[?, Base.Writer, ?, *], A], a: A): Json = schema match
-    case Base.Schema.Optional(self)      => a.map(apply(self, _)).getOrElse(Json.Null)
-    case Base.Schema.Required.Root(data) => apply(data, a)
-    case Base.Schema.Root(data)          => apply(data, a)
+  def apply0[A](
+      schema: Base.Schema[
+        Base.Data[Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, ?, *], *], *], Base.Writer, ?, *],
+        A
+      ],
+      a: A
+  ): Json = schema match
+    case Base.Schema.Optional(self)      => a.map(apply0(self, _)).getOrElse(Json.Null)
+    case Base.Schema.Required.Root(data) => apply0(data, a)
+    case Base.Schema.Root(data)          => apply0(data, a)
 
-  def apply[A](data: Base.Data[?, Base.Writer, ?, A], a: A): Json = data match
-    case schema: Base.Primitive[A]           => JsonPrimitiveEncoder(schema, a)
-    case schema: Base.Collection[?, ?, ?, A] => ???
+  def apply0[A](
+      data: Base.Data[
+        Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, ?, *], *], *],
+        Base.Writer,
+        ?,
+        A
+      ],
+      a: A
+  ): Json = data match
+    case schema: Base.Primitive[A] => JsonPrimitiveEncoder(schema, a)
+    case schema: Base.Collection[Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, ?, *], *], *], ?, ?, A] =>
+      Json.fromValues(JsonCollectionEncoder(schema, a))
