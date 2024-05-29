d9b2aa6e357433be8e02cc3257649aff87bf455c
Niklas Klein <mail@taig.io>
2024-05-29 10:04:07 +0200

    Save progress

--- modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonCollectionEncoder.scala
+++ modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonCollectionEncoder.scala
@@ -1,18 +1,19 @@
 package io.taig.otter.json.circe
 
 import io.taig.otter as Base
+import io.taig.otter.Plain.*
 import io.circe.Json
 import cats.syntax.all.*
 import cats.Id as Identity
 
 object JsonCollectionEncoder:
   def apply[A](
-      data: Base.Collection[
-        Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, ?, *], *], *],
-        ?,
-        ?,
-        A
-      ],
+      data: Base.Collection[Identity, Base.Writer, Parent.Writer, A],
       a: A
   ): Vector[Json] = data match
-    case Base.Collection.Root(schema) => a.map(JsonEncoder.apply(schema, _)).some
+    case Base.Collection.Root(schema) =>
+      // val x: AsSchema[Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, Parent.Writer, *], *], A]] = schema
+      val x: AsSchema[Base.Writer[Base.Schema[Base.Data[Identity, Base.Writer, ?, *], *], A]] = schema
+      // a.map(JsonEncoder.apply0(x, _))
+      ???
+    // a.map(JsonEncoder.apply0(schema, _)).some
