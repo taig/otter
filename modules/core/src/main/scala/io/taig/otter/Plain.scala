dd2577ad67d9c9feaf51e0496cc946279be45f16
Niklas Klein <mail@taig.io>
2024-05-30 22:51:46 +0200

    Save progress

--- modules/core/src/main/scala/io/taig/otter/Plain.scala
+++ modules/core/src/main/scala/io/taig/otter/Plain.scala
@@ -1,15 +1,28 @@
 package io.taig.otter
 
 import io.taig.otter as Base
+import io.taig.otter.Collection.Root
+import io.taig.otter.Isomorphic.Root
 
 object Plain extends Dsl:
-  final override type AsSchema[A] = A
-  final override type AsCollection[A] = A
-  final override type AsPrimitive[A] = A
-  final override type AsTuple[A] = A
+  final override type AsSchema[+A] = A
+  final override type AsCollection[+A] = A
+  final override type AsPrimitive[+A] = A
+  final override type AsTuple[+A] = A
 
   override def primitive[A](tpe: Type[A]): Primitive.Required[A] =
     Base.Isomorphic.Root(Base.Required(Base.Primitive.Root(tpe)))
 
   override def collection[F[a] <: Parent.Isomorphic[a], A](schema: F[A]): Collection.Of[F[A], Vector[A]] =
     Base.Isomorphic.Root(Base.Required(Base.Collection.Root(schema)))
+
+  val x: Schema[Vector[String]] = collection(string)
+
+  x match
+    case Isomorphic.Root(fa) =>
+      fa match
+        case Required(fa) =>
+          fa match
+            case Base.Collection.Root(fa) =>
+              val x: Schema[String] = fa
+              ???
--- modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonCollectionEncoder.scala
+++ modules/json-circe/src/main/scala/io/taig/otter/json/circe/JsonCollectionEncoder.scala
@@ -6,9 +6,11 @@ import io.circe.Json
 import cats.syntax.all.*
 
 object JsonCollectionEncoder:
-  def apply[A](data: Base.Collection[Parent.Writer.Any, A], a: A): Vector[Json] =
-    data match
-      case data: Base.Collection.Root[Parent.Writer, ?] => apply1(data, a)
-      // case Base.Collection.Root(schema)  => a.map(JsonEncoder.apply0(schema, _))
+  def apply[A](data: Base.Collection[Parent.Writer[?], A], a: A): Vector[Json] = data match
+    // case Base.Collection.Root(schema) =>
+    //   val x: Parent.Writer[?] = schema
+    //   ???
+    case data: Base.Collection.Root[Base.Writer[AsSchema, Base.Optional, Base.Schema, ?, *], ?] => ???
+    // case data: Base.Collection[Base.Writer[AsSchema, Base.Optional, Base.Schema, ?, ?], ?]      => ???
 
   def apply1[A](data: Base.Collection.Root[Parent.Writer, A], a: Vector[A]): Vector[Json] = ???
