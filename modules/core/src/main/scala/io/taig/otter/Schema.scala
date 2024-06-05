d2a3504866aa5d8f618c8fafdea7f3712f969680
Niklas Klein <mail@taig.io>
2024-06-05 09:35:08 +0200

    ops ops ops

--- modules/core/src/main/scala/io/taig/otter/Schema.scala
+++ modules/core/src/main/scala/io/taig/otter/Schema.scala
@@ -59,6 +59,58 @@ object Schema:
       override def ivalidate[V1, V2, C](validation: SchemaValidation[B, V1, V2, C])(f: C => B): Schema[M, A, C] =
         self.ivalidate(validation)(f)
 
+sealed trait Collection[+M, +A, B] extends Schema[M, A, B], Collection.Reader[M, A, B], Collection.Writer[M, A, B]:
+  override def modify[N](f: M => N): Collection[N, A, B] = ???
+  final override def optional: Collection[M, A, Option[B]] = Collection.Optional(this)
+  final def ivalidate[V1, V2, C](validation: SchemaValidation[B, V1, V2, C])(f: C => B): Collection[M, A, C] =
+    Collection.Modify(this, validation, f)
+
+object Collection:
+  sealed trait Reader[+M, +A, +B] extends Schema.Reader[M, A, B]:
+    def constraints: Chain[Constraint[?]]
+    override def modify[N](f: M => N): Collection.Reader[N, A, B] = ???
+    def optional: Collection.Reader[M, A, Option[B]] = Reader.Optional(this)
+    final def validate[V1, V2, C](validation: SchemaValidation[B, V1, V2, C]): Collection.Reader[M, A, C] =
+      Reader.Modify(this, validation)
+
+  object Reader:
+    final case class Modify[M, A, B, V1, V2, C](
+        self: Collection.Reader[M, A, B],
+        validation: SchemaValidation[B, V1, V2, C]
+    ) extends Collection.Reader[M, A, C]:
+      override def constraints: Chain[Constraint[?]] = validation.constraints
+
+    final case class Optional[M, A, B](self: Collection.Reader[M, A, B]) extends Collection.Reader[M, A, Option[B]]:
+      export self.constraints
+
+    final case class Root[M, +A <: Schema.Reader[M, ?, B], B](schema: A) extends Collection.Reader[M, A, Vector[B]]:
+      override def constraints: Chain[Constraint[?]] = Chain.empty
+
+  sealed trait Writer[+M, +A, -B] extends Schema.Writer[M, A, B]:
+    final def contramap[C](f: C => B): Collection.Writer[M, A, C] = Writer.Modify(this, f)
+    override def modify[N](f: M => N): Collection.Writer[N, A, B] = ???
+    def optional: Collection.Writer[M, A, Option[B]] = Writer.Optional(this)
+
+  object Writer:
+    final case class Modify[M, A, B, C](self: Collection.Writer[M, A, B], f: C => B) extends Collection.Writer[M, A, C]
+
+    final case class Optional[M, A, B](self: Collection.Writer[M, A, B]) extends Collection.Writer[M, A, Option[B]]
+
+    final case class Root[M, +A <: Schema.Writer[M, ?, B], B](schema: A) extends Collection.Writer[M, A, Vector[B]]
+
+  final case class Modify[M, A, B, V1, V2, C](
+      self: Collection[M, A, B],
+      validation: SchemaValidation[B, V1, V2, C],
+      f: C => B
+  ) extends Collection[M, A, C]:
+    override def constraints: Chain[Constraint[?]] = self.constraints ++ validation.constraints
+
+  final case class Optional[M, A, B](self: Collection[M, A, B]) extends Collection[M, A, Option[B]]:
+    export self.constraints
+
+  final case class Root[M, +A <: Schema[M, ?, B], B](schema: A) extends Collection[M, A, Vector[B]]:
+    override def constraints: Chain[Constraint[?]] = Chain.empty
+
 sealed trait Primitive[+M, A] extends Schema[M, Nothing, A], Primitive.Reader[M, A], Primitive.Writer[M, A]:
   override def modify[N](f: M => N): Primitive[N, A] = ???
   final override def optional: Primitive[M, Option[A]] = Primitive.Optional(this)
--- modules/core/src/main/scala/io/taig/otter/SchemaOps.scala
+++ modules/core/src/main/scala/io/taig/otter/SchemaOps.scala
@@ -1,6 +1,7 @@
 package io.taig.otter
 
 import cats.syntax.all.*
+import cats.data.Chain
 
 trait SchemaOps[F[_, _, _], G[_, _, _], C[_, _, _], T[_, _, _]]:
   extension [M, A, B](self: F[M, A, B])
@@ -12,6 +13,12 @@ trait SchemaOps[F[_, _, _], G[_, _, _], C[_, _, _], T[_, _, _]]:
     def optional: G[M, A, Option[B]]
     def toTuple: T[M, self.type, B]
 
+trait CollectionOps[F[_, _, _], T[_, _, _]] extends SchemaOps[F, F, F, T]:
+  extension [M, A, B](self: F[M, A, B]) def schema: Schema[M, ?, ?]
+
 trait PrimitiveOps[F[_, _], G[_, _], C[_, _, _], T[_, _, _]]
     extends SchemaOps[[m, _, a] =>> F[m, a], [m, _, a] =>> G[m, a], C, T]:
   extension [M, A](self: F[M, A]) def tpe: Type[?]
+
+trait TupleOps[F[_, _, _], C[_, _, _]] extends SchemaOps[F, F, C, F]:
+  extension [M, A, B](self: F[M, A, B]) def schemas: Chain[Schema[M, ?, ?]]
