b548a70537733ae14477914ef896d2da2a969e49
Niklas Klein <mail@taig.io>
2024-06-10 20:39:29 +0200

    Save progress

--- modules/core/src/main/scala/io/taig/otter/CollectionBuilder.scala
+++ modules/core/src/main/scala/io/taig/otter/CollectionBuilder.scala
@@ -1,11 +1,28 @@
 package io.taig.otter
 
-abstract class CollectionBuilder[F[_]]:
-  def to[A](a: Vector[A]): F[A]
-
-  def from[A](fa: F[A]): Vector[A]
+trait CollectionBuilder[F[_]] extends CollectionBuilder.Reader[F], CollectionBuilder.Writer[F]
 
 object CollectionBuilder:
-  val vector: CollectionBuilder[Vector] = new CollectionBuilder[Vector]:
-    override inline def to[A](a: Vector[A]): Vector[A] = a
-    override inline def from[A](fa: Vector[A]): Vector[A] = fa
+  trait Reader[F[_]]:
+    def to[A](values: Vector[A]): F[A]
+
+  object Reader:
+    def apply[F[_]](f: [A] => Vector[A] => F[A]): CollectionBuilder.Reader[F] = new Reader[F]:
+      override def to[A](values: Vector[A]): F[A] = f(values)
+
+  trait Writer[F[_]]:
+    def from[A](fa: F[A]): Vector[A]
+
+  object Writer:
+    def apply[F[_]](f: [A] => F[A] => Vector[A]): CollectionBuilder.Writer[F] = new Writer[F]:
+      override def from[A](fa: F[A]): Vector[A] = f(fa)
+
+  def apply[F[_]](f: [A] => Vector[A] => F[A], g: [A] => F[A] => Vector[A]): CollectionBuilder[F] =
+    new CollectionBuilder[F]:
+      override def to[A](values: Vector[A]): F[A] = f(values)
+      override def from[A](fa: F[A]): Vector[A] = g(fa)
+
+  val vector: CollectionBuilder[Vector] = CollectionBuilder(
+    [A] => (fa: Vector[A]) => fa,
+    [A] => (values: Vector[A]) => values
+  )
