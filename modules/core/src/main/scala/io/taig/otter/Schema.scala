251bb06fd3e50832f1410f53edef0596a00b79e1
Niklas Klein <mail@taig.io>
2024-06-27 12:58:48 +0200

    Schema seems somewhat fine now

--- modules/core/src/main/scala/io/taig/otter/Schema.scala
+++ modules/core/src/main/scala/io/taig/otter/Schema.scala
@@ -2,7 +2,6 @@ package io.taig.otter
 
 import cats.syntax.all.*
 import io.taig.otter.Schema.Reader
-import io.taig.otter.validation.Validation
 import cats.data.Chain
 
 sealed trait Schema[F[+_], +A, B] extends Schema.Reader[F, A, B], Schema.Writer[F, A, B]:
@@ -84,89 +83,92 @@ object Collection:
     override def constraints: Chain[Constraint.Collection] = self.constraints ++ transformation.validation.constraints
 
 sealed trait Primitive[F[+_], A] extends Schema[F, Nothing, A], Primitive.Reader[F, A], Primitive.Writer[F, A]:
-  override def imap[C](f: A => C)(g: C => A): Primitive[F, C] = ??? // ivalidate(Validation.lift(f))(g)
-  // def ivalidate[B, C, D](
-  //     validation: Validation[A, Constraint.Primitive[(Schema.Writer[F, ?, B], B)], (Schema.Writer[F, ?, C], C), D]
-  // )(f: D => A): Primitive[F, D] = Primitive.Validate(this, validation, f)
+  override def imap[C](f: A => C)(g: C => A): Primitive[F, C] = transform(Transformation.lift(f)(g))
   final override def optional: Primitive[F, Option[A]] = Primitive.Optional(this)
+  def transform[B, C, D](transformation: PrimitiveTransformation[F, A, B, C, D]): Primitive[F, D] =
+    Primitive.Transform(this, transformation)
 
 object Primitive:
   sealed trait Required[F[+_], A]
       extends Primitive[F, A],
         Primitive.Required.Reader[F, A],
         Primitive.Required.Writer[F, A]:
-    final override def imap[C](f: A => C)(g: C => A): Primitive.Required[F, C] = ??? // ivalidate(Validation.lift(f))(g)
-    // final override def ivalidate[B, C, D](
-    //     validation: Validation[A, Constraint.Primitive[(Schema.Writer[F, ?, B], B)], (Schema.Writer[F, ?, C], C), D]
-    // )(f: D => A): Primitive.Required[F, D] = Primitive.Required.Validate(this, validation, f)
+    final override def imap[C](f: A => C)(g: C => A): Primitive.Required[F, C] = transform(Transformation.lift(f)(g))
+    override def transform[B, C, D](transformation: PrimitiveTransformation[F, A, B, C, D]): Primitive.Required[F, D] =
+      Required.Transform(this, transformation)
 
   object Required:
     sealed trait Reader[F[+_], +A] extends Primitive.Reader[F, A]:
-      override def map[C](f: A => C): Primitive.Required.Reader[F, C] = ??? // validate(Validation.lift(f))
-      // final override def validate[B, C, D](
-      //     validation: Validation[A, Constraint.Primitive[
-      //       (Schema.Writer[F, ?, B], B)
-      //     ], (Schema.Writer[F, ?, C], C), D]
-      // ): Primitive.Required.Reader[F, D] = Reader.Validate(this, validation)
+      final override def map[C](f: A => C): Primitive.Required.Reader[F, C] = transform(Transformation.Reader.lift(f))
+      final override def transform[A1 >: A, B, C, D](
+          transformation: PrimitiveTransformation.Reader[F, A1, B, C, D]
+      ): Primitive.Required.Reader[F, D] = Reader.Transform(this, transformation)
 
     object Reader:
-      final case class Validate[F[+_], A, B, C, D](
+      final case class Transform[F[+_], A, B, C, D](
           self: Primitive.Required.Reader[F, A],
-          validation: Validation[A, Constraint.Primitive[(Schema.Writer[F, ?, B], B)], (Schema.Writer[F, ?, C], C), D]
+          transformation: PrimitiveTransformation.Reader[F, A, B, C, D]
       ) extends Primitive.Required.Reader[F, D]:
         export self.tpe
-        override def constraints: Chain[Constraint.Primitive[?]] = self.constraints ++ validation.constraints
+        override def constraints: Chain[Constraint.Primitive[?]] =
+          self.constraints ++ transformation.validation.constraints
 
     sealed trait Writer[F[+_], -A] extends Primitive.Writer[F, A]:
-      final override def contramap[B](f: B => A): Primitive.Required.Writer[F, B] = Writer.Modify(this, f)
+      final override def contramap[B](f: B => A): Primitive.Required.Writer[F, B] =
+        transform(Transformation.Writer(f))
+      final override def transform[A1 <: A, B](
+          transformation: Transformation.Writer[A1, B]
+      ): Primitive.Required.Writer[F, B] = Writer.Transform(this, transformation)
 
     object Writer:
-      final case class Modify[F[+_], A, B](self: Primitive.Required.Writer[F, A], f: B => A)
-          extends Primitive.Required.Writer[F, B]:
+      final case class Transform[F[+_], A, B](
+          self: Primitive.Required.Writer[F, A],
+          transformation: Transformation.Writer[A, B]
+      ) extends Primitive.Required.Writer[F, B]:
         export self.tpe
 
     final case class Root[F[+_], A](tpe: Type[A]) extends Primitive.Required[F, A]:
       override def constraints: Chain[Constraint.Primitive[?]] = Chain.empty
 
-    final case class Validate[F[+_], A, B, C, D](
+    final case class Transform[F[+_], A, B, C, D](
         self: Primitive.Required[F, A],
-        validation: Validation[A, Constraint.Primitive[
-          (Schema.Writer[F, ?, B], B)
-        ], (Schema.Writer[F, ?, C], C), D],
-        f: D => A
+        transformation: PrimitiveTransformation[F, A, B, C, D]
     ) extends Primitive.Required[F, D]:
       export self.tpe
-      override def constraints: Chain[Constraint.Primitive[?]] = self.constraints ++ validation.constraints
+      override def constraints: Chain[Constraint.Primitive[?]] =
+        self.constraints ++ transformation.validation.constraints
 
   sealed trait Reader[F[+_], +A] extends Schema.Reader[F, Nothing, A]:
     def constraints: Chain[Constraint.Primitive[?]]
-    override def map[C](f: A => C): Primitive.Reader[F, C] = ??? // validate(Validation.lift(f))
+    override def map[C](f: A => C): Primitive.Reader[F, C] = transform(Transformation.Reader.lift(f))
     override def optional: Primitive.Reader[F, Option[A]] = Reader.Optional(this)
     def tpe: Type[?]
-    // def validate[B, C, D](
-    //     validation: Validation[A, Constraint.Primitive[(Schema.Writer[F, ?, B], B)], (Schema.Writer[F, ?, C], C), D]
-    // ): Primitive.Reader[F, D] = Reader.Validate(this, validation)
+    def transform[A1 >: A, B, C, D](
+        transformation: PrimitiveTransformation.Reader[F, A1, B, C, D]
+    ): Primitive.Reader[F, D] = Reader.Transform(this, transformation)
 
   object Reader:
-    final case class Validate[F[+_], A, B, C, D](
+    final case class Transform[F[+_], A, B, C, D](
         self: Primitive.Reader[F, A],
-        validation: Validation[A, Constraint.Primitive[
-          (Schema.Writer[F, ?, B], B)
-        ], (Schema.Writer[F, ?, C], C), D]
+        transformation: PrimitiveTransformation.Reader[F, A, B, C, D]
     ) extends Primitive.Reader[F, D]:
       export self.tpe
-      override def constraints: Chain[Constraint.Primitive[?]] = self.constraints ++ validation.constraints
+      override def constraints: Chain[Constraint.Primitive[?]] =
+        self.constraints ++ transformation.validation.constraints
 
     final case class Optional[F[+_], A](self: Primitive.Reader[F, A]) extends Primitive.Reader[F, Option[A]]:
       export self.{constraints, tpe}
 
   sealed trait Writer[F[+_], -A] extends Schema.Writer[F, Nothing, A]:
-    def contramap[C](f: C => A): Primitive.Writer[F, C] = Writer.Modify(this, f)
-    def optional: Primitive.Writer[F, Option[A]] = Writer.Optional(this)
+    override def contramap[B](f: B => A): Primitive.Writer[F, B] = transform(Transformation.Writer(f))
+    override def optional: Primitive.Writer[F, Option[A]] = Writer.Optional(this)
     def tpe: Type[?]
+    def transform[A1 <: A, B](transformation: Transformation.Writer[A1, B]): Primitive.Writer[F, B] =
+      Writer.Transform(this, transformation)
 
   object Writer:
-    final case class Modify[F[+_], A, B](self: Primitive.Writer[F, A], f: B => A) extends Primitive.Writer[F, B]:
+    final case class Transform[F[+_], A, B](self: Primitive.Writer[F, A], transformation: Transformation.Writer[A, B])
+        extends Primitive.Writer[F, B]:
       export self.tpe
 
     final case class Optional[F[+_], A](self: Primitive.Writer[F, A]) extends Primitive.Writer[F, Option[A]]:
@@ -175,15 +177,12 @@ object Primitive:
   final case class Optional[F[+_], A](self: Primitive[F, A]) extends Primitive[F, Option[A]]:
     export self.{constraints, tpe}
 
-  final case class Validate[F[+_], A, B, C, D](
+  final case class Transform[F[+_], A, B, C, D](
       self: Primitive[F, A],
-      validation: Validation[A, Constraint.Primitive[
-        (Schema.Writer[F, ?, B], B)
-      ], (Schema.Writer[F, ?, C], C), D],
-      f: D => A
+      transformation: PrimitiveTransformation[F, A, B, C, D]
   ) extends Primitive[F, D]:
     export self.tpe
-    override def constraints: Chain[Constraint.Primitive[?]] = self.constraints ++ validation.constraints
+    override def constraints: Chain[Constraint.Primitive[?]] = self.constraints ++ transformation.validation.constraints
 
 sealed trait Union[F[+_], +A, B] extends Schema[F, A, B], Union.Reader[F, A, B], Union.Writer[F, A, B]:
   override def imap[C](f: B => C)(g: C => B): Union[F, A, C] = ???
--- modules/core/src/main/scala/io/taig/otter/SchemaTransformation.scala
+++ modules/core/src/main/scala/io/taig/otter/SchemaTransformation.scala
@@ -1,11 +1,26 @@
 package io.taig.otter
 
-type SchemaTransformation[F[+_], A, +B <: Constraint.Any[?], C, D] = Transformation[A, B, (F[Schema[F, ?, C]], C), D]
+type SchemaTransformation[F[+_], +G[a] <: Constraint.Any[a], A, B, C, D] = Transformation[
+  A,
+  G[(F[Schema[F, ?, B]], B)],
+  (F[Schema[F, ?, C]], C),
+  D
+]
 
 object SchemaTransformation:
-  type Reader[F[+_], A, +B <: Constraint.Any[?], C, D] = Transformation.Reader[A, B, (F[Schema[F, ?, C]], C), D]
+  type Reader[F[+_], +G[a] <: Constraint.Any[a], A, B, C, D] = Transformation.Reader[
+    A,
+    G[(F[Schema[F, ?, B]], B)],
+    (F[Schema[F, ?, C]], C),
+    D
+  ]
 
-type CollectionTransformation[F[+_], A, B, C] = SchemaTransformation[F, A, Constraint.Collection, B, C]
+type CollectionTransformation[F[+_], A, B, C] = SchemaTransformation[F, [_] =>> Constraint.Collection, A, Nothing, B, C]
 
 object CollectionTransformation:
-  type Reader[F[+_], A, B, C] = SchemaTransformation.Reader[F, A, Constraint.Collection, B, C]
+  type Reader[F[+_], A, B, C] = SchemaTransformation.Reader[F, [_] =>> Constraint.Collection, A, Nothing, B, C]
+
+type PrimitiveTransformation[F[+_], A, B, C, D] = SchemaTransformation[F, Constraint.Primitive, A, B, C, D]
+
+object PrimitiveTransformation:
+  type Reader[F[+_], A, B, C, D] = SchemaTransformation.Reader[F, Constraint.Primitive, A, B, C, D]
