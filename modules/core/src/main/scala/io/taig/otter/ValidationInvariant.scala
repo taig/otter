57740da0c95f39178d8f315997c8d841d4b74398
Niklas Klein <mail@taig.io>
2024-07-01 20:21:29 +0200

    Complete instances

--- modules/core/src/main/scala/io/taig/otter/ValidationInvariant.scala
+++ modules/core/src/main/scala/io/taig/otter/ValidationInvariant.scala
@@ -7,7 +7,7 @@ trait ValidationInvariant[Constraint[_], Actual[_], F[_]] extends Invariant[F]:
   override def imap[A, B](fa: F[A])(f: A => B)(g: B => A): F[B] = fa.ivalidate(Validation.lift(f))(g)
 
   extension [A](fa: F[A])
-    def ivalidate[B, C, D](validation: Validation[A, Constraint[B], Actual[C], D])(g: D => A): F[D]
+    def ivalidate[B, C, D](validation: Validation[A, Constraint[B], Actual[C], D])(f: D => A): F[D]
 
     final def ivalidate_[B, C](validation: Validation[A, Constraint[B], Actual[C], Unit]): F[A] =
       ivalidate(validation.tap)(identity)
