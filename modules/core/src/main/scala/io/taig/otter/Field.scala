c147e0fa666340b519fb038e68c8b4fa551d3a9f
Niklas Klein <mail@taig.io>
2024-07-03 22:49:13 +0200

    Simplify Field

--- modules/core/src/main/scala/io/taig/otter/Field.scala
+++ modules/core/src/main/scala/io/taig/otter/Field.scala
@@ -8,26 +8,20 @@ import io.taig.otter.Decoder.Result
 import cats.kernel.Eq
 
 sealed trait Field[+F[+_], +A, B] extends Field.Reader[F, A, B], Field.Writer[F, A, B]:
+  def name: String
   def schema: F[Schema[F, ?, ?]]
   override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field[G, ?, B]
 
 object Field:
   sealed trait Reader[+F[+_], +A, +B]:
-    def matches(value: String): Decoder.Result[String, Boolean]
     def name: String
     def schema: F[Schema.Reader[F, ?, ?]]
     def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field.Reader[G, ?, B]
 
   object Reader:
-    final case class Root[F[+_], A: Show: Eq, B <: F[Schema.Reader[F, ?, C]], C](
-        identifier: A,
-        key: Value.Required[Identity, ?, A],
-        schema: B
-    ) extends Field.Reader[F, B, C]:
-      override def matches(value: String): Result[String, Boolean] =
-        StringDecoder(key, value).map(_ === identifier)
-      override def name: String = StringEncoder(key, identifier)
-      override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field.Reader[G, ?, C] =
+    final case class Root[F[+_], +A <: F[Schema.Reader[F, ?, B]], B](name: String, schema: A)
+        extends Field.Reader[F, A, B]:
+      override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field.Reader[G, ?, B] =
         copy(schema = fK(schema).map(_.translate(fK)))
 
   sealed trait Writer[+F[+_], +A, -B]:
@@ -36,22 +30,11 @@ object Field:
     def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field.Writer[G, ?, B]
 
   object Writer:
-    final case class Root[F[+_], A, B <: F[Schema.Writer[F, ?, C]], C](
-        identifier: A,
-        key: Value.Required.Writer[Identity, ?, A],
-        schema: B
-    ) extends Field.Writer[F, B, C]:
-      override def name: String = StringEncoder(key, identifier)
-      override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field.Writer[G, ?, C] =
+    final case class Root[F[+_], +A <: F[Schema.Writer[F, ?, B]], B](name: String, schema: A)
+        extends Field.Writer[F, A, B]:
+      override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field.Writer[G, ?, B] =
         copy(schema = fK(schema).map(_.translate(fK)))
 
-  final case class Root[F[+_], A: Show: Eq, B <: F[Schema[F, ?, C]], C](
-      identifier: A,
-      key: Value.Required[Identity, ?, A],
-      schema: B
-  ) extends Field[F, B, C]:
-    override def matches(value: String): Result[String, Boolean] =
-      StringDecoder(key, value).map(_ === identifier)
-    override def name: String = StringEncoder(key, identifier)
-    override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field[G, ?, C] =
+  final case class Root[F[+_], +A <: F[Schema[F, ?, B]], B](name: String, schema: A) extends Field[F, A, B]:
+    override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Field[G, ?, B] =
       copy(schema = fK(schema).map(_.translate(fK)))
--- modules/json-circe/src/main/scala/io/taig/otter/json/circe/FieldJsonEncoder.scala
+++ modules/json-circe/src/main/scala/io/taig/otter/json/circe/FieldJsonEncoder.scala
@@ -7,4 +7,5 @@ import io.taig.otter.StringEncoder
 
 object FieldJsonEncoder:
   def apply[A](field: Field.Writer[A], a: A): (String, Json) = field match
-    case Base.Field.Root(_, _, schema) => field.name -> JsonEncoder(schema, a)
+    case Base.Field.Root(name, schema)        => name -> JsonEncoder(schema, a)
+    case Base.Field.Writer.Root(name, schema) => name -> JsonEncoder(schema, a)
