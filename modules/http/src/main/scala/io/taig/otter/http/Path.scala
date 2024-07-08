a91c316c5ea1b478352182c97eecbb636a400235
Niklas Klein <mail@taig.io>
2024-07-08 10:38:09 +0200

    Path translation

--- modules/http/src/main/scala/io/taig/otter/http/Path.scala
+++ modules/http/src/main/scala/io/taig/otter/http/Path.scala
@@ -1,16 +1,23 @@
 package io.taig.otter.http
 
 import cats.data.Chain
+import cats.Functor
 
 sealed trait Path[+F[+_], +A] extends Product, Serializable:
   def toSegments: Chain[Segment[F, ?]]
+  def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Path[G, A]
 
 object Path:
   final case class Combine[F[+_], A, B](left: Path[F, A], right: Path[F, B]) extends Path[F, (A, B)]:
     override def toSegments: Chain[Segment[F, ?]] = left.toSegments ++ right.toSegments
+    override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Path[G, (A, B)] =
+      copy(left = left.translate(fK), right = right.translate(fK))
 
   case object Empty extends Path[Nothing, Unit]:
     override def toSegments: Chain[Nothing] = Chain.empty
+    override def translate[G[+_]: Functor](fK: [A] => Nothing => G[A]): Path[Nothing, Unit] = this
 
   final case class One[F[+_], A](segment: Segment[F, A]) extends Path[F, A]:
     override def toSegments: Chain[Segment[F, ?]] = Chain.one(segment)
+    override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Path[G, A] =
+      copy(segment = segment.translate(fK))
--- modules/http/src/main/scala/io/taig/otter/http/Segment.scala
+++ modules/http/src/main/scala/io/taig/otter/http/Segment.scala
@@ -1,10 +1,16 @@
 package io.taig.otter.http
 
 import io.taig.otter.Value
+import cats.Functor
+import cats.syntax.all.*
 
-sealed trait Segment[+F[+_], +A]
+sealed trait Segment[+F[+_], +A]:
+  def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Segment[G, A]
 
 object Segment:
-  final case class Static(name: String) extends Segment[Nothing, Unit]
+  final case class Static(name: String) extends Segment[Nothing, Unit]:
+    override def translate[G[+_]: Functor](fK: [A] => Nothing => G[A]): Segment[Nothing, Unit] = this
 
-  final case class Parameter[F[+_], A](name: String, schema: F[Value.Required.Reader[F, ?, A]])
+  final case class Parameter[F[+_], A](name: String, schema: F[Value.Required.Reader[F, ?, A]]) extends Segment[F, A]:
+    override def translate[G[+_]: Functor](fK: [A] => F[A] => G[A]): Segment[G, A] =
+      copy(schema = fK(schema).map(_.translate(fK)))
