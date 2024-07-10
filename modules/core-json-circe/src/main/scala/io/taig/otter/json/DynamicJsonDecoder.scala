9362a2bfc9230224507bda60b439db47eed6a849
Niklas Klein <mail@taig.io>
2024-07-10 17:49:42 +0200

    Fully implement dynamic codecs

--- modules/core-json-circe/src/main/scala/io/taig/otter/json/DynamicJsonDecoder.scala
+++ modules/core-json-circe/src/main/scala/io/taig/otter/json/DynamicJsonDecoder.scala
@@ -8,7 +8,13 @@ import cats.syntax.all.*
 
 object DynamicJsonDecoder:
   def apply[A](schema: Dynamic.Reader[Json, A], json: Json): Decoder.Result[Json, A] = schema match
-    case Base.Dynamic.Root()                 => json.valid
-    case Base.Dynamic.Reader.Root()          => json.valid
-    case Base.Dynamic.Reader.Optional(_)     => ???
-    case Base.Dynamic.Reader.Transform(_, _) => ???
+    case Base.Dynamic.Root()                    => json.valid
+    case Base.Dynamic.Reader.Root()             => json.valid
+    case Base.Dynamic.Reader.Optional(self)     => optional(self, json)
+    case Base.Dynamic.Reader.Transform(self, f) => transform(self, f, json)
+
+  def optional[A](self: Dynamic.Reader[Json, A], json: Json): Decoder.Result[Json, Option[A]] =
+    if json.isNull then none.valid else DynamicJsonDecoder(self, json).map(_.some)
+
+  def transform[A, B](self: Dynamic.Reader[Json, A], f: A => B, json: Json): Decoder.Result[Json, B] =
+    DynamicJsonDecoder(self, json).map(f)
--- modules/core-json-circe/src/main/scala/io/taig/otter/json/DynamicJsonEncoder.scala
+++ modules/core-json-circe/src/main/scala/io/taig/otter/json/DynamicJsonEncoder.scala
@@ -6,7 +6,13 @@ import io.circe.Json
 
 object DynamicJsonEncoder:
   def apply[A](schema: Dynamic.Writer[Json, A], a: A): Json = schema match
-    case Base.Dynamic.Root()                 => a
-    case Base.Dynamic.Writer.Root()          => a
-    case Base.Dynamic.Writer.Optional(_)     => ???
-    case Base.Dynamic.Writer.Transform(_, _) => ???
+    case Base.Dynamic.Root()                    => a
+    case Base.Dynamic.Writer.Root()             => a
+    case Base.Dynamic.Writer.Optional(self)     => optional(self, a)
+    case Base.Dynamic.Writer.Transform(self, f) => transform(self, f, a)
+
+  def optional[A](self: Dynamic.Writer[Json, A], a: Option[A]): Json =
+    a.fold(Json.Null)(DynamicJsonEncoder(self, _))
+
+  def transform[A, B](self: Dynamic.Writer[Json, A], f: B => A, b: B): Json =
+    DynamicJsonEncoder(self, f(b))
