c3eba4aa5c24e9a4e7ba43ad951f89b1c1e30239
Niklas Klein <mail@taig.io>
2024-07-16 17:00:42 +0200

    Dictionary

--- modules/core/src/main/scala/io/taig/otter/Dictionary.scala
+++ modules/core/src/main/scala/io/taig/otter/Dictionary.scala
@@ -1,52 +1,36 @@
-// package io.taig.otter
+package io.taig.otter
 
-// import cats.syntax.all.*
-// import io.taig.otter.Codec.Result
-// import cats.data.Chain
-// import io.taig.otter.validation.Violations
-// import io.taig.otter.validation.Violation
+import cats.syntax.all.*
+import io.taig.otter.Codec.Result
+import cats.data.Chain
+import io.taig.otter.validation.Violations
+import io.taig.otter.validation.Violation
 
-// abstract class Dictionary[+O, A] extends Codec[O, A]:
-//   self =>
+abstract class Dictionary[+O, A] extends Codec[O, A]:
+  self =>
 
-//   override def imap[B](f: A => B)(g: B => A): Dictionary[O, B] = new Dictionary[O, B]:
-//     export self.metadata
-//     override def decodeObject(data: Option[Data.Object]): Codec.Result[Data, B] =
-//       self.decodeOption(data).map(f)
-//     override def encodeObject(b: B): Option[Data.Object] = self.encodeObject(g(b))
+  final override def metadata(f: Metadata => Metadata): Dictionary[O, A] = new Dictionary[O, A]:
+    export self.{decode, default, encode}
+    override def metadata: Metadata = f(self.metadata)
 
-//   override def optional: Dictionary[O, Option[A]] = new Dictionary[O, Option[A]]:
-//     export self.metadata
-//     override def decodeObject(data: Option[Data.Object]): Codec.Result[Data, Option[A]] =
-//       data.fold(none.valid)(_ => self.decodeOption(data).map(_.some))
-//     override def encodeObject(a: Option[A]): Option[Data.Object] = a.flatMap(self.encodeObject)
+  final override def default(f: Option[A] => Option[A]): Dictionary[O, A] = new Dictionary[O, A]:
+    export self.{encode, metadata}
+    override def default: Option[A] = f(self.default)
+    override def decode(data: Data): Codec.Result[A] = (data, default) match
+      case (Data.Null, Some(default)) => default.valid
+      case _                          => self.decode(data)
 
-//   override def update(f: Metadata => Metadata): Dictionary[O, A] = new Dictionary[O, A]:
-//     export self.{decodeObject, encodeObject}
-//     override def metadata: Metadata = f(self.metadata)
+  final override def imap[B](f: A => B)(g: B => A): Dictionary[O, B] = ???
 
-//   final override def decodeOption(data: Option[Data.Value]): Codec.Result[Data, A] = data match
-//     case Some(data: Data.Object) => decodeObject(Some(data))
-//     case Some(data) => Violations.rootNec(Violation(Constraint.Type("object"), Data.String(data.name))).invalid
-//     case None       => decodeObject(None)
+  final override def optional: Dictionary[O, Option[A]] = ???
 
-//   def decodeObject(data: Option[Data.Object]): Codec.Result[Data, A]
-
-//   final override def encodeOption(a: A): Option[Data.Value] = encodeObject(a)
-
-//   def encodeObject(a: A): Option[Data.Object]
-
-// object Dictionary:
-//   def apply[A, B](key: Value.Required[?, A], value: Codec[?, B]): Dictionary[value.type, Chain[(A, B)]] =
-//     new Dictionary[value.type, Chain[(A, B)]]:
-//       override def metadata: Metadata = Metadata.Empty
-//       override def decodeObject(data: Option[Data.Object]): Result[Data, Chain[(A, B)]] = data
-//         .toValid(Violations.rootNec(Violation(Constraint.Type("object"), Data.String("null"))))
-//         .andThen(_.values.traverse { case (a, b) =>
-//           (
-//             key.parseValue(a).leftMap(_.map(_.bimap(_.map(Data.String.apply), Data.String.apply))),
-//             value.decode(b)
-//           ).tupled
-//         })
-//       override def encodeObject(abs: Chain[(A, B)]): Option[Data.Object] =
-//         Data.Object(abs.map { case (a, b) => (key.printValue(a), value.encode(b)) }).some
+object Dictionary:
+  def apply[A, B](key: Value.Required[?, A], value: Codec[?, B]): Dictionary[value.type, Chain[(A, B)]] =
+    new Dictionary:
+      override def metadata: Metadata = Metadata.Empty
+      override def default: Option[Chain[(A, B)]] = None
+      override def decode(data: Data): Result[Chain[(A, B)]] = data.toObject
+        .toValid(Violations.rootNec(Violation(Constraint.Type("object"), actual = Data.String(data.name))))
+        .andThen(_.values.traverse { case (a, b) => (key.parseValue(a), value.decode(b)).tupled })
+      override def encode(abs: Chain[(A, B)]): Data =
+        Data.Object(abs.map { case (a, b) => (key.printValue(a), value.encode(b)) })
