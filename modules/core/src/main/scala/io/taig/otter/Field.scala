f038ed4dbb9640fb0c1889ad1232beb3b10fd492
Niklas Klein <mail@taig.io>
2024-07-17 09:24:34 +0200

    Fields

--- modules/core/src/main/scala/io/taig/otter/Field.scala
+++ modules/core/src/main/scala/io/taig/otter/Field.scala
@@ -1,16 +1,44 @@
 package io.taig.otter
 
 import cats.syntax.all.*
-import scala.Product as SProduct
+import cats.data.Chain
+import io.taig.otter.Keys.*
 
-sealed trait Field[+O, A] extends SProduct, Serializable:
-  final def imap[B](f: A => B)(g: B => A): Field[O, B] = Field.Transform(this, f, g)
-  def metadata: Metadata
+sealed abstract class Field[+O, A]:
+  self =>
+
   def name: String
-  def schema: Codec[?, ?]
+
+  def codec: Codec[?, ?]
+
+  def metadata: Metadata
+
+  final def modifyMetadata(f: Metadata => Metadata): Field[O, A] = new Field[O, A]:
+    export self.{codec, decode, encodeValue, name}
+    override def metadata: Metadata = f(self.metadata)
+
+  final def imap[B](f: A => B)(g: B => A): Field[O, B] = new Field[O, B]:
+    export self.{codec, metadata, name}
+    override def decode(values: Chain[(String, Data)]): Codec.Result[(Chain[(String, Data)], B)] =
+      self.decode(values).map(_.map(f))
+    override def encodeValue(b: B): Data = self.encodeValue(g(b))
+
+  def decode(values: Chain[(String, Data)]): Codec.Result[(Chain[(String, Data)], A)]
+
+  final def encode(a: A): Option[(String, Data)] = (metadata(nulls).getOrElse(Null.Default), encodeValue(a)) match
+    case (Null.Hide, Data.Null) => None
+    case (_, data)              => (name, data).some
+
+  protected def encodeValue(a: A): Data
 
 object Field:
-  final case class Root[F, O <: Codec[?, A], A](metadata: Metadata, name: String, schema: O) extends Field[O, A]
+  def apply[A](name: String, codec: Codec[?, A]): Field[codec.type, A] =
+    val _name = name
+    val _codec = codec
 
-  final case class Transform[F, O, A, B](self: Field[O, A], f: A => B, g: B => A) extends Field[O, B]:
-    export self.{metadata, name, schema}
+    new Field[codec.type, A]:
+      override def name: String = _name
+      override def codec: Codec[?, A] = _codec
+      override def metadata: Metadata = Metadata.Empty
+      override def decode(values: Chain[(String, Data)]): Codec.Result[(Chain[(String, Data)], A)] = ???
+      override def encodeValue(a: A): Data = codec.encode(a)
