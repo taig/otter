ef8eb50889f9580eef48f26a14e38f5774f86778
Niklas Klein <mail@taig.io>
2024-07-24 10:05:51 +0200

    Save good progress

--- modules/core/src/main/scala/io/taig/otter/Enumeration.scala
+++ modules/core/src/main/scala/io/taig/otter/Enumeration.scala
@@ -1,92 +1,54 @@
-// package io.taig.otter
+package io.taig.otter
 
-// import cats.syntax.all.*
-// import cats.Invariant
-// import io.taig.enumeration.ext.Mapping
-// import io.taig.otter.Codec.Result
-// import io.taig.otter.validation.Violations
-// import io.taig.otter.validation.Violation
+import cats.syntax.all.*
+import cats.Invariant
+import io.taig.enumeration.ext.Mapping
+import io.taig.otter.Codec.Result
+import io.taig.otter.validation.Violations
+import io.taig.otter.validation.Violation
 
-// abstract class Enumeration[+O, A] extends Value[O, A]:
-//   self =>
+abstract class Enumeration[+O <: Data.Optional[Data.Primitive], A] extends Codec[O, A]:
+  self =>
 
-//   def codec: Value[?, ?]
+  def codec: Codec[?, ?]
 
-//   override def modifyMetadata(f: Metadata => Metadata): Enumeration[O, A] = new Enumeration[O, A]:
-//     export self.{codec, decode, default, encode}
-//     override def metadata: Metadata = f(self.metadata)
+  override def modifyMetadata(f: Metadata => Metadata): Enumeration[O, A] = new Enumeration[O, A]:
+    export self.{codec, decode, default, encode}
+    override def metadata: Metadata = f(self.metadata)
 
-//   final override def modifyDefault(f: Option[A] => Option[A]): Enumeration[O, A] = new Enumeration[O, A]:
-//     export self.{codec, encode, metadata}
-//     override def default: Option[A] = f(self.default)
-//     override def decode(data: Data): Codec.Result[A] = (data, default) match
-//       case (Data.Null, Some(default)) => default.valid
-//       case _                          => self.decode(data)
-//     // override def parse(value: Option[String]): Codec.Result[A] = (value, default) match
-//     //   case (None, Some(default)) => default.valid
-//     //   case _                     => self.parse(value)
+  final override def modifyDefault(f: Option[A] => Option[A]): Enumeration[O, A] = new Enumeration[O, A]:
+    export self.{codec, encode, metadata}
+    override def default: Option[A] = f(self.default)
+    override def decode(data: Data): Codec.Result[A] = (data, default) match
+      case (Data.Null, Some(default)) => default.valid
+      case _                          => self.decode(data)
 
-//   override def imap[B](f: A => B)(g: B => A): Enumeration[O, B] = new Enumeration[O, B]:
-//     export self.{codec, metadata}
-//     override def default: Option[B] = self.default.map(f)
-//     override def decode(data: Data): Codec.Result[B] = self.decode(data).map(f)
-//     override def encode(b: B): Format[this.type] = self.encode(g(b))
-//     // override def parse(value: Option[String]): Codec.Result[B] = self.parse(value).map(f)
-//     // override def print(b: B): Option[String] = self.print(g(b))
+  override def imap[B](f: A => B)(g: B => A): Enumeration[O, B] = new Enumeration[O, B]:
+    export self.{codec, metadata}
+    override def default: Option[B] = self.default.map(f)
+    override def decode(data: Data): Codec.Result[B] = self.decode(data).map(f)
+    override def encode(b: B): O = self.encode(g(b))
 
-//   final override def optional: Enumeration[O, Option[A]] = new Enumeration[O, Option[A]]:
-//     export self.{codec, metadata}
-//     override def default: Option[Option[A]] = self.default.map(_.some)
-//     override def decode(data: Data): Codec.Result[Option[A]] =
-//       data.toValue.fold(default.flatten.valid)(_ => self.decode(data).map(_.some))
-//     // override def parse(value: Option[String]): Codec.Result[Option[A]] =
-//     //   value.fold(default.flatten.valid)(_ => self.parse(value).map(_.some))
-//     // override def print(a: Option[A]): Option[String] = a.flatMap(self.print)
-//     override def encode(a: Option[A]): Format[this.type] = a.map(self.encode).getOrElse(Data.Null)
+  final override def optional: Enumeration[Data.Optional[O], Option[A]] = new Enumeration[Data.Optional[O], Option[A]]:
+    export self.{codec, metadata}
+    override def default: Option[Option[A]] = self.default.map(_.some)
+    override def decode(data: Data): Codec.Result[Option[A]] =
+      data.toValue.fold(default.flatten.valid)(_ => self.decode(data).map(_.some))
+    override def encode(a: Option[A]): Data.Optional[O] = a.map(self.encode).getOrElse(Data.Null)
 
-// object Enumeration:
-//   abstract class Required[+O, A] extends Enumeration[O, A], Value.Required[O, A]:
-//     self =>
+object Enumeration:
+  def apply[O <: Data.Primitive, A, B](of: Codec[O, A], mapping: Mapping[B, A]): Enumeration[O, B] =
+    new Enumeration[O, B]:
+      override def codec: Codec[?, ?] = of
+      override def metadata: Metadata = Metadata.Empty
+      override def default: Option[B] = None
+      override def decode(data: Data): Codec.Result[B] = of
+        .decode(data)
+        .andThen: a =>
+          mapping
+            .unapply(a)
+            .toValid(Violations.rootNec(Violation(Constraint.OneOf(mapping.values.map(encode)), actual = data)))
+      override def encode(b: B): O = of.encode(mapping(b))
 
-//     override def codec: Value.Required[?, ?]
-
-//     final override def modifyMetadata(f: Metadata => Metadata): Enumeration.Required[O, A] = new Required[O, A]:
-//       export self.{codec, decodeValue, default, encode}
-//       override def metadata: Metadata = f(self.metadata)
-
-//     final override def imap[B](f: A => B)(g: B => A): Enumeration.Required[O, B] = new Required[O, B]:
-//       export self.{codec, metadata}
-//       override def default: Option[B] = self.default.map(f)
-//       override def decodeValue(data: Data.Value): Codec.Result[B] = self.decodeValue(data).map(f)
-//       override def encode(b: B): Format[this.type] = self.encode(g(b))
-//       // override def parseValue(value: String): Codec.Result[B] = self.parseValue(value).map(f)
-//       // override def printValue(b: B): String = self.printValue(g(b))
-
-//   object Required:
-//     given [O]: Invariant[Enumeration.Required[O, *]] with
-//       override def imap[A, B](fa: Enumeration.Required[O, A])(f: A => B)(g: B => A): Enumeration.Required[O, B] =
-//         fa.imap(f)(g)
-
-//   def apply[A, B](of: Value.Required[?, A], mapping: Mapping[B, A]): Enumeration.Required[of.type, B] =
-//     new Enumeration.Required[of.type, B]:
-//       override def codec: Value.Required[?, A] = of
-//       override def metadata: Metadata = Metadata.Empty
-//       override def default: Option[B] = None
-//       override def decodeValue(data: Data.Value): Codec.Result[B] = of
-//         .decodeValue(data)
-//         .andThen: a =>
-//           mapping
-//             .unapply(a)
-//             .toValid(Violations.rootNec(Violation(Constraint.OneOf(mapping.values.map(encode)), actual = data)))
-//       // override def parseValue(value: String): Codec.Result[B] = codec
-//       //   .parseValue(value)
-//       //   .andThen: a =>
-//       //     mapping
-//       //       .unapply(a)
-//       //       .toValid(
-//       //         Violations.rootNec(Violation(Constraint.OneOf(mapping.values.map(encode)), actual = Data.String(value)))
-//       //       )
-//       override def encode(b: B): Format[this.type] = of.encode(mapping(b))
-
-//   given [O]: Invariant[Enumeration[O, *]] with
-//     override def imap[A, B](fa: Enumeration[O, A])(f: A => B)(g: B => A): Enumeration[O, B] = fa.imap(f)(g)
+  given [O <: Data.Optional[Data.Primitive]]: Invariant[Enumeration[O, *]] with
+    override def imap[A, B](fa: Enumeration[O, A])(f: A => B)(g: B => A): Enumeration[O, B] = fa.imap(f)(g)
