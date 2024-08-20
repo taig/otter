8fa5f13cd87b026318ee9de657c6b659bbb64222
Niklas Klein <mail@taig.io>
2024-08-20 14:29:19 +0200

    Save progress

--- modules/http/src/main/scala/io/taig/otter/http/package.scala
+++ modules/http/src/main/scala/io/taig/otter/http/package.scala
@@ -2,12 +2,11 @@ package io.taig.otter.http
 
 import cats.syntax.all.*
 import java.nio.charset.Charset
-import java.nio.charset.UnsupportedCharsetException
-import java.nio.charset.IllegalCharsetNameException
 
 private def loadCharset(name: String): Option[Charset] =
   try Charset.forName(name).some
   catch {
-    case _: UnsupportedCharsetException => none
-    case _: IllegalCharsetNameException => none
+    case _: IllegalArgumentException => none
+    // Scala.js chokes on this
+    // case _: IllegalCharsetNameException => none
   }
