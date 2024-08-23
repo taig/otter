5dca7c20412dca75d38f1d86b5d1a2c2dc0a6d68
Niklas Klein <mail@taig.io>
2024-08-23 10:15:47 +0200

    Tests are good, almost ready to merge

--- modules/sample-app/src/test/scala/io/taig/otter/sample/routes/BooksRoutesTest.scala
+++ modules/sample-app/src/test/scala/io/taig/otter/sample/routes/BooksRoutesTest.scala
@@ -35,59 +35,14 @@ final class BooksRoutesTest extends SampleSuite:
       assertEquals(obtained, expected = Nil)
     }
 
-//   app.test(endpoints.books.post, "single"): context =>
-//     for
-//       librarian <- context.api.librarian
-//       obtained <- context.client
-//         .submit(endpoints.books.post, session = librarian, NonEmptyChain(fixtures.book.main()))
-//         .assertSuccess
-//       expected <- context.client.submit(endpoints.books.get, input = ()).assertAuthenticated
-//     yield {
-//       assertEquals(obtained.toChain, expected)
-//     }
-
-//   app.test(endpoints.books.post, description = "multiple"): context =>
-//     for
-//       librarian <- context.api.librarian
-//       obtained <- context.client
-//         .submit(
-//           endpoints.books.post,
-//           session = librarian,
-//           NonEmptyChain(
-//             fixtures.book.main(index = 1),
-//             fixtures.book.main(index = 2),
-//             fixtures.book.main(index = 3)
-//           )
-//         )
-//         .assertSuccess
-//       expected <- context.client.submit(endpoints.books.get, input = ()).assertAuthenticated
-//     yield {
-//       assertEquals(obtained.toChain, expected)
-//     }
-
-//   app.test(endpoints.books.post, description = "isbn conflict"): context =>
-//     for
-//       librarian <- context.api.librarian
-//       isbn = fixtures.isbn()
-//       book = fixtures.book.main(isbn = isbn)
-//       _ <- context.client.submit(endpoints.books.post, session = librarian.some, NonEmptyChain(book)).assertSuccess
-//       obtained <- context.client
-//         .submit(endpoints.books.post, session = librarian.some, NonEmptyChain(book))
-//         .assertError
-//     yield {
-//       assertEquals(obtained, Post.IsbnConflict(isbn))
-//     }
-
-//     app.test(endpoints.books.post, description = "isbn conflict (multiple)"): context =>
-//       for
-//         librarian <- context.client
-//           .submit(endpoints.librarians.self.sessions.post, Librarian.Create.Default.toLogin)
-//           .assertSuccess
-//         isbn = fixtures.isbn()
-//         book = fixtures.book.main(isbn = isbn)
-//         obtained <- context.client
-//           .submit(endpoints.books.post, session = librarian, NonEmptyChain(book, book))
-//           .assertError
-//       yield {
-//         assertEquals(obtained, Post.IsbnConflict(isbn))
-//       }
+  app.test(endpoint.books.post(), description = "isbn conflict"): context =>
+    for
+      librarian <- context.api.librarian
+      book = fixtures.book.create(isbn = fixtures.isbn())
+      _ <- context.client.fallible(endpoint.books.post(), session = librarian.some, book).assertSuccess
+      obtained <- context.client
+        .fallible(endpoint.books.post(), session = librarian.some, book)
+        .assertError
+    yield {
+      assertEquals(obtained, Error.IsbnConflict)
+    }
