4f5f16a24907a27ce6d9a5b406c4e90a14f348e3
Niklas Klein <mail@taig.io>
2024-12-21 21:09:34 +0100

    Good collection implementation

--- modules/typescript/src/main/scala/io/taig/otter/TypescriptPrinter.scala
+++ modules/typescript/src/main/scala/io/taig/otter/TypescriptPrinter.scala
@@ -4,18 +4,43 @@ import cats.syntax.all.*
 
 object TypescriptPrinter:
   def apply(codec: Codec[?, ?]): String = codec match
-    case codec: Primitive[?, ?] => apply(codec)
+    case codec: Collection[?, ?]  => apply(codec)
+    case codec: Dictionary[?, ?]  => apply(codec)
+    case codec: Enumeration[?, ?] => apply(codec)
+    case codec: Primitive[?, ?]   => apply(codec)
+    case codec: Record[?, ?]      => apply(codec)
+    case codec                    => s"<Unsupported codec: ${codec.getClass.getSimpleName}>"
 
-  def apply(data: Data.Primitive): String = data match
-    case Data.String(value)  => s"\"$value\""
-    case Data.Boolean(true)  => "true"
-    case Data.Boolean(false) => "false"
-    case Data.Number(value)  => String.valueOf(value)
+  def apply(data: Data.Primitive): String = Printers(data, quoted = true)
 
   def apply(codec: Primitive[?, ?]): String = codec.tpe match
-    case Type.Boolean => "boolean"
-    case Type.Number  => "number"
-    case Type.String  => "string"
+    case Type.Boolean => "z.boolean()"
+    case Type.Number  => "z.number()"
+    case Type.String  => "z.string()"
 
-  def apply(enumeration: Enumeration[?, ?]): String =
-    enumeration.data.map(apply).mkString_(" | ")
+  def apply(codec: Enumeration[?, ?]): String =
+    val values = codec.data.map(apply).mkString_(", ")
+    s"z.enum([$values])"
+
+  def apply(codec: Record[?, ?]): String =
+    val fields = codec.fields.map(field => s"""  "${field.name}": ${apply(field.codec.value)}""")
+    s"""z.object({
+       |${fields.mkString_(",\n")}
+       |})""".stripMargin
+
+  def apply(codec: Collection[?, ?]): String =
+    val minItems = codec.constraints.collectFirst { case Constraint.Collection.MinItems(value) => value }
+    val maxItems = codec.constraints.collectFirst { case Constraint.Collection.MaxItems(value) => value }
+    val uniqueItems = codec.constraints.collectFirst { case Constraint.Collection.UniqueItems => true }.getOrElse(false)
+    val nonEmpty = minItems.fold(false)(_ >= 1)
+
+    val tpe = if uniqueItems then s"z.set(${apply(codec.codec.value)})" else s"z.array(${apply(codec.codec.value)})"
+
+    val size = (minItems, maxItems) match
+      case (Some(min), Some(max)) if min === max => s".length($min)"
+      case _ =>
+        minItems.filter(_ > 1).map(min => s".min($min)").orEmpty + maxItems.map(max => s".max($max)").orEmpty
+
+    tpe + (if nonEmpty then ".nonempty()" else "") + size
+
+  def apply(codec: Dictionary[?, ?]): String = s"z.map(${apply(codec.key.value)}, ${apply(codec.codec.value)})"
--- modules/typescript/src/test/scala/io/taig/otter/TypescriptPrinterTest.scala
+++ modules/typescript/src/test/scala/io/taig/otter/TypescriptPrinterTest.scala
@@ -1,18 +1,19 @@
 package io.taig.otter
 
 import io.taig.otter.Dsl.*
+import cats.syntax.all.*
 
 final class TypescriptPrinterTest extends OtterSuite:
   test("primitive"):
-    assertEq(obtained = TypescriptPrinter(bigDecimal), expected = "number")
-    assertEq(obtained = TypescriptPrinter(bigInt), expected = "number")
-    assertEq(obtained = TypescriptPrinter(boolean), expected = "boolean")
-    assertEq(obtained = TypescriptPrinter(double), expected = "number")
-    assertEq(obtained = TypescriptPrinter(float), expected = "number")
-    assertEq(obtained = TypescriptPrinter(int), expected = "number")
-    assertEq(obtained = TypescriptPrinter(long), expected = "number")
-    assertEq(obtained = TypescriptPrinter(string), expected = "string")
-    assertEq(obtained = TypescriptPrinter(uuid), expected = "string")
+    assertEq(obtained = TypescriptPrinter(bigDecimal), expected = "z.number()")
+    assertEq(obtained = TypescriptPrinter(bigInt), expected = "z.number()")
+    assertEq(obtained = TypescriptPrinter(boolean), expected = "z.boolean()")
+    assertEq(obtained = TypescriptPrinter(double), expected = "z.number()")
+    assertEq(obtained = TypescriptPrinter(float), expected = "z.number()")
+    assertEq(obtained = TypescriptPrinter(int), expected = "z.number()")
+    assertEq(obtained = TypescriptPrinter(long), expected = "z.number()")
+    assertEq(obtained = TypescriptPrinter(string), expected = "z.string()")
+    assertEq(obtained = TypescriptPrinter(uuid), expected = "z.string()")
 
   test("enumeration"):
     enum Data:
@@ -25,4 +26,48 @@ final class TypescriptPrinterTest extends OtterSuite:
       case Data.B => "b"
       case Data.C => "c"
 
-    assertEq(obtained = TypescriptPrinter(codec), expected = """"a" | "b" | "c"""")
+    assertEq(obtained = TypescriptPrinter(codec), expected = """z.enum(["a", "b", "c"])""")
+
+  test("record"):
+    val codec = field("a", string) :* field("b", int) :* field("c", boolean)
+
+    assertEq(
+      obtained = TypescriptPrinter(codec),
+      expected = """z.object({
+                   |  "a": z.string(),
+                   |  "b": z.number(),
+                   |  "c": z.boolean()
+                   |})""".stripMargin
+    )
+
+  test("collection"):
+    val codec = collection.list(string)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.array(z.string())")
+
+  test("collection: nonEmpty"):
+    val codec = collection.nonEmptyList(int)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.array(z.number()).nonempty()")
+
+  test("collection: minItems"):
+    val codec = collection.list(int, minItems = 3.some)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.array(z.number()).nonempty().min(3)")
+
+  test("collection: maxItems"):
+    val codec = collection.list(int, maxItems = 10.some)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.array(z.number()).max(10)")
+
+  test("collection: length"):
+    val codec = collection.list(int, minItems = 10.some, maxItems = 10.some)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.array(z.number()).nonempty().length(10)")
+
+  test("collection: uniqueItems"):
+    val codec = collection.set(int)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.set(z.number())")
+
+  test("collection: minItems & maxItems"):
+    val codec = collection.nonEmptyList(int, minItems = 3.some, maxItems = 10.some)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.array(z.number()).nonempty().min(3).max(10)")
+
+  test("dictionary"):
+    val codec = dictionary.map(string, int)
+    assertEq(obtained = TypescriptPrinter(codec), expected = "z.map(z.string(), z.number())")
