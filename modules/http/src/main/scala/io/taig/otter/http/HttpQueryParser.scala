fac3be13f5db581320f7b75d30b4df8373098eea
Niklas Klein <mail@taig.io>
2025-04-14 09:51:39 +0200

    Save progress on query decoder, it's gonna be tricky

--- modules/http/src/main/scala/io/taig/otter/http/HttpQueryParser.scala
+++ modules/http/src/main/scala/io/taig/otter/http/HttpQueryParser.scala
@@ -2,36 +2,56 @@ package io.taig.otter.http
 
 import cats.data.Validated
 import io.taig.otter.*
-import cats.data.Chain
 import cats.syntax.all.*
 
 final class HttpQueryParser(explode: Boolean, style: Query.Style):
-  def apply[A](name: String, codec: Http.Query[A], values: Chain[(String, Option[String])]): Validated[Violations, A] =
-    codec match
-      case codec: Http.Query.Array[A]    => ???
-      case codec: Http.Query.Object[A]   => ???
-      case codec: Http.Query.Optional[A] => apply(name, codec, values)
-      case codec: Http.Query.Value[A]    => ??? // HttpQueryValueParser(codec, value)
+  def apply[A](
+      name: String,
+      codec: Http.Query[A],
+      values: List[(String, Option[String])]
+  ): Validated[Violations, (List[(String, Option[String])], A)] = codec match
+    case codec: Http.Query.Array[A]    => ???
+    case codec: Http.Query.Object[A]   => ???
+    case codec: Http.Query.Optional[A] => apply(name, codec, values)
+    case codec: Http.Query.Value[A]    => apply(name, codec, values)
 
   def apply[A](
       name: String,
       codec: Http.Query.Optional[A],
-      values: Chain[(String, Option[String])]
-  ): Validated[Violations, A] =
-    apply(name, codec = codec.self, values)
+      values: List[(String, Option[String])]
+  ): Validated[Violations, (List[(String, Option[String])], A)] = apply(name, codec = codec.self, values)
 
   def apply[A](
       name: String,
       codec: Optional[Http.Query, A],
-      values: Chain[(String, Option[String])]
-  ): Validated[Violations, A] =
-    codec match
-      case Optional.Modify(self, f, _) => apply(name, codec = self, values).map(f)
-      case Optional.Default(codec, default, _) =>
-        if values.exists((key, _) => key === name)
-        then apply(name, codec = codec.value, values)
-        else default.valid
-      case Optional.Nullable(codec, _) =>
-        if values.exists((key, _) => key === name)
-        then apply(name, codec = codec.value, values).map(_.some)
-        else None.valid
+      values: List[(String, Option[String])]
+  ): Validated[Violations, (List[(String, Option[String])], A)] = codec match
+    case Optional.Modify(self, f, _) => apply(name, codec = self, values).map(_.map(f))
+    case Optional.Default(codec, default, _) =>
+      val (remainders, result) = values.collectFirstWithRemainders { case (`name`, value) => value }
+
+      result
+        .toValid(Violations.rootNec(Violation.required))
+        .andThen:
+          case Some(_) => apply(name, codec = codec.value, values)
+          case None    => (remainders, default).valid
+    case Optional.Nullable(codec, _) =>
+      val (remainders, result) = values.collectFirstWithRemainders { case (`name`, value) => value }
+
+      result
+        .toValid(Violations.rootNec(Violation.required))
+        .andThen:
+          case Some(_) => apply(name, codec = codec.value, values).map(_.map(_.some))
+          case None    => (remainders, none).valid
+
+  def apply[A](
+      name: String,
+      codec: Http.Query.Value[A],
+      values: List[(String, Option[String])]
+  ): Validated[Violations, (List[(String, Option[String])], A)] =
+    val (remainders, result) = values.collectFirstWithRemainders { case (`name`, value) => value }
+
+    result.flatten
+      .toValid(Violations.rootNec(Violation.required))
+      .andThen(HttpQueryValueParser(codec, _).tupleLeft(remainders))
+      .leftMap(name /: _)
