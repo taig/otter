1a44f401233bae00c29ec443098eb711c398e9ec
Niklas Klein <mail@taig.io>
2025-04-27 12:55:38 +0200

    Restore SessionApiSchema

--- modules/sample-api/src/main/scala/io/taig/otter/sample/api/schema/IsbnApiSchema.scala
+++ modules/sample-api/src/main/scala/io/taig/otter/sample/api/schema/IsbnApiSchema.scala
@@ -10,7 +10,7 @@ opaque type IsbnApiSchema = Long
 object IsbnApiSchema:
   extension (self: IsbnApiSchema) def toLong: Long = self
 
-  def unsafe(value: Long): IsbnApiSchema = value
+  def apply(value: Long): IsbnApiSchema = value
 
   val codec: Json.Primitive[IsbnApiSchema] =
     long(minimum = comparison(1L).some, maximum = comparison(9999999999999L).some)
--- modules/sample-api/src/main/scala/io/taig/otter/sample/api/schema/SessionApiSchema.scala
+++ modules/sample-api/src/main/scala/io/taig/otter/sample/api/schema/SessionApiSchema.scala
@@ -1,23 +1,28 @@
-// package io.taig.otter.sample.api.schema
+package io.taig.otter.sample.api.schema
 
-// import cats.syntax.all.*
-// import io.taig.otter.sample.api.Dsl.*
+import cats.syntax.all.*
+import io.taig.otter.Json
+import io.taig.otter.sample.api.Dsl.*
+import io.taig.otter.sample.api.Dsl.json.*
 
-// import java.util.UUID
+import java.util.UUID
 
-// opaque type SessionApiSchema = UUID
+opaque type SessionApiSchema = UUID
 
-// object SessionApiSchema:
-//   extension (self: SessionApiSchema) def toUUID: UUID = self
+object SessionApiSchema:
+  extension (self: SessionApiSchema) def toUUID: UUID = self
 
-//   def apply(value: UUID): SessionApiSchema = value
+  def apply(value: UUID): SessionApiSchema = value
 
-//   def codec(prefix: String): Primitive[SessionApiSchema] = parser("session") { value =>
-//     Option
-//       .when(value.startsWith(prefix) && value.length > prefix.length + 1)(value.substring(prefix.length))
-//       .flatMap: value =>
-//         try UUID.fromString(value).some
-//         catch { case _: IllegalArgumentException => none }
-//   }(uuid => prefix + uuid.show)
+  def codec(prefix: String): Json.Primitive[SessionApiSchema] = parser("session") { value =>
+    Either
+      .cond(
+        test = value.startsWith(prefix) && value.length > prefix.length + 1,
+        right = value.substring(prefix.length),
+        left = s"Prefix invalid: '$prefix'"
+      )
+      .flatMap: value =>
+        Either.catchOnly[IllegalArgumentException](UUID.fromString(value)).leftMap(_.getMessage)
+  }(uuid => prefix + uuid.show)
 
-//   val codec: Primitive[SessionApiSchema] = codec(prefix = "")
+  val codec: Json.Primitive[SessionApiSchema] = codec(prefix = "")
