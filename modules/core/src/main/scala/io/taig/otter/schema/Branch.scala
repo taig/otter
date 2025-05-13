a27d4ca26f30e133824ad1090dd6e261789b776f
Niklas Klein <mail@taig.io>
2025-05-14 00:56:59 +0200

    The clean up train is coming

--- modules/core/src/main/scala/io/taig/otter/schema/Branch.scala
+++ modules/core/src/main/scala/io/taig/otter/schema/Branch.scala
@@ -12,6 +12,10 @@ import scala.Double as SDouble
 import scala.Float as SFloat
 import scala.Int as SInt
 import scala.Long as SLong
+import io.taig.otter.schema.Primitive.Boolean.Component as PrimitiveBooleanComponent
+import io.taig.otter.schema.Primitive.Number.Component as PrimitiveNumberComponent
+import io.taig.otter.schema.Primitive.String.Component as PrimitiveStringComponent
+import io.taig.otter.schema.Primitive.Component as PrimitiveComponent
 
 sealed abstract class Branch[+S[_], +T[_], A] extends Schema[T, A]:
   def key: Reference.Constant[S, ?]
@@ -39,45 +43,49 @@ object Branch:
     override def modifyMetadata(f: Metadata => Metadata): Branch[S, T, B] = copy(metadata = f(metadata))
     override def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Branch[S, U, B] = copy(value = value.mapK(fK))
 
-  trait Shape[Self[_], -Key[_], -Value[_], Sum[_]] extends Schema.Shape[Self], Invariant.Coproduct.Lift[Self, Sum]:
+  trait Shape[Self[_], Key[_], Value[_], Sum[_]] extends Schema.Shape[Self], Invariant.Coproduct.Lift[Self, Sum]:
     def branch[A, B](name: A, key: => Key[A], value: => Value[B]): Self[B]
 
-    extension [A](self: Self[A]) def toSum: Sum[A]
+    extension [A](self: Self[A])
+      def key: Reference.Constant[Key, ?]
+      def value: Reference[Value, ?]
+      def toSum: Sum[A]
 
-  // trait Component[+Self[_], -Key[_], -Value[_], Sum[_]](using codec: Codec.Branch[Self, Key, Value, Sum]):
-  //   final def branch[A, B](name: A, key: => Key[A], value: => Value[B]): Self[B] = codec.branch(name, key, value)
+  trait Component[+Self[_], -Key[_], -Value[_], Sum[_]](using shape: Branch.Shape[Self, Key, Value, Sum]):
+    final def branch[A, B](name: A, key: => Key[A], value: => Value[B]): Self[B] = shape.branch(name, key, value)
 
-  // object Component:
-  //   trait Primitive[+Self[_], Key[_], -Value[_], Record[_]]
-  //       extends Component.Primitive.Boolean[Self, Key, Value, Record],
-  //         Component.Primitive.Number[Self, Key, Value, Record],
-  //         Component.Primitive.String[Self, Key, Value, Record]:
-  //     override def key: PrimitiveDsl[Key]
+  object Component:
+    trait Primitive[+Self[_], Key[_], -Value[_], Record[_]]
+        extends Component.Primitive.Boolean[Self, Key, Value, Record],
+          Component.Primitive.Number[Self, Key, Value, Record],
+          Component.Primitive.String[Self, Key, Value, Record]:
+      override def key: PrimitiveComponent[Key]
 
-  //   object Primitive:
-  //     trait Boolean[+Self[_], Key[_], -Value[_], Sum[_]] extends Component[Self, Key, Value, Sum]:
-  //       def key: PrimitiveDsl.Boolean[Key]
+    object Primitive:
+      trait Boolean[+Self[_], Key[_], -Value[_], Sum[_]] extends Branch.Component[Self, Key, Value, Sum]:
+        def key: PrimitiveBooleanComponent[Key]
 
-  //       final def branch[A](name: SBoolean, codec: => Value[A]): Self[A] =
-  //         branch(name, key = key.boolean, value = codec)
+        final def branch[A](name: SBoolean, codec: => Value[A]): Self[A] =
+          branch(name, key = key.boolean, value = codec)
 
-  //     trait Number[+Self[_], Key[_], -Value[_], Sum[_]] extends Component[Self, Key, Value, Sum]:
-  //       def key: PrimitiveDsl.Number[Key]
+      trait Number[+Self[_], Key[_], -Value[_], Sum[_]] extends Component[Self, Key, Value, Sum]:
+        def key: PrimitiveNumberComponent[Key]
 
-  //       final def branch[A](name: BigDecimal, codec: => Value[A]): Self[A] =
-  //         branch(name, key = key.bigDecimal, value = codec)
-  //       final def branch[A](name: BigInt, codec: => Value[A]): Self[A] = branch(name, key = key.bigInteger, value = codec)
-  //       final def branch[A](name: JBigDecimal, codec: => Value[A]): Self[A] =
-  //         branch(name, key = key.jBigDecimal, value = codec)
-  //       final def branch[A](name: JBigInteger, codec: => Value[A]): Self[A] =
-  //         branch(name, key = key.jBigInteger, value = codec)
-  //       final def branch[A](name: SDouble, codec: => Value[A]): Self[A] = branch(name, key = key.double, value = codec)
-  //       final def branch[A](name: SFloat, codec: => Value[A]): Self[A] = branch(name, key = key.float, value = codec)
-  //       final def branch[A](name: SInt, codec: => Value[A]): Self[A] = branch(name, key = key.int, value = codec)
-  //       final def branch[A](name: SLong, codec: => Value[A]): Self[A] = branch(name, key = key.long, value = codec)
+        final def branch[A](name: BigDecimal, codec: => Value[A]): Self[A] =
+          branch(name, key = key.bigDecimal, value = codec)
+        final def branch[A](name: BigInt, codec: => Value[A]): Self[A] =
+          branch(name, key = key.bigInteger, value = codec)
+        final def branch[A](name: JBigDecimal, codec: => Value[A]): Self[A] =
+          branch(name, key = key.jBigDecimal, value = codec)
+        final def branch[A](name: JBigInteger, codec: => Value[A]): Self[A] =
+          branch(name, key = key.jBigInteger, value = codec)
+        final def branch[A](name: SDouble, codec: => Value[A]): Self[A] = branch(name, key = key.double, value = codec)
+        final def branch[A](name: SFloat, codec: => Value[A]): Self[A] = branch(name, key = key.float, value = codec)
+        final def branch[A](name: SInt, codec: => Value[A]): Self[A] = branch(name, key = key.int, value = codec)
+        final def branch[A](name: SLong, codec: => Value[A]): Self[A] = branch(name, key = key.long, value = codec)
 
-  //     trait String[+Self[_], Key[_], -Value[_], Sum[_]] extends Component[Self, Key, Value, Sum]:
-  //       def key: PrimitiveDsl.String[Key]
+      trait String[+Self[_], Key[_], -Value[_], Sum[_]] extends Component[Self, Key, Value, Sum]:
+        def key: PrimitiveStringComponent[Key]
 
-  //       final def branch[A](name: JString, codec: => Value[A]): Self[A] =
-  //         branch(name, key = key.string, value = codec)
+        final def branch[A](name: JString, codec: => Value[A]): Self[A] =
+          branch(name, key = key.string, value = codec)
