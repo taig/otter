d416a2bd5286a62b90d70123ce739d8c58a8f7ec
Niklas Klein <mail@taig.io>
2025-05-13 17:18:44 +0200

    Differentiate between Sum and Union

--- modules/core/src/main/scala/io/taig/otter/Branch.scala
+++ modules/core/src/main/scala/io/taig/otter/Branch.scala
@@ -1,40 +1,29 @@
 package io.taig.otter
 
 import cats.~>
-import cats.arrow.FunctionK
 
 sealed abstract class Branch[+S[_], +T[_], A] extends Product with Serializable:
+  def key: Reference.Constant[S, ?]
+  def value: Reference[T, ?]
+
   def metadata: Metadata
   def modifyMetadata(f: Metadata => Metadata): Branch[S, T, A]
 
   final def imap[B](f: A => B)(g: B => A): Branch[S, T, B] = Branch.Modify(self = this, f, g)
 
-  def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Branch[S, U, A] = ???
+  def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Branch[S, U, A]
 
 object Branch:
-  final private[otter] case class Explicit[+S[_], A, B](
-      key: Reference.Constant[S, A],
-      value: Reference[S, B],
-      metadata: Metadata
-  ) extends Branch[S, S, B]:
-    override def modifyMetadata(f: Metadata => Metadata): Branch[S, S, B] = copy(metadata = f(metadata))
-    // override def mapK[S1[a] >: S[a], U[_]](fK: S1 ~> U): Branch[U, U, B] = copy(key = key.mapK(fK), value = value.mapK(fK))
-
-  final private[otter] case class Keyed[+S[_], +T[_], A](metadata: Metadata) extends Branch[S, T, A]:
-    override def modifyMetadata(f: Metadata => Metadata): Branch[S, T, A] = copy(metadata = f(metadata))
-    override def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Branch[S, U, A] = ???
-
-  final private[otter] case class Merged[+S[_], A](metadata: Metadata) extends Branch[S, S, A]:
-    override def modifyMetadata(f: Metadata => Metadata): Branch[S, S, A] = copy(metadata = f(metadata))
-    override def mapK[S1[a] >: S[a], U[_]](fK: S1 ~> U): Branch[S, U, A] = ???
-
   final private[otter] case class Modify[S[_], T[_], A, B](self: Branch[S, T, A], f: A => B, g: B => A)
       extends Branch[S, T, B]:
-    export self.metadata
+    export self.{key, metadata, value}
     override def modifyMetadata(f: Metadata => Metadata): Branch[S, T, B] = copy(self = self.modifyMetadata(f))
     override def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Branch[S, U, B] = copy(self = self.mapK(fK))
 
-  // final private[otter] case class Untagged[+S[_], A](name: String, codec: Reference[S, A], metadata: Metadata)
-  //     extends Branch[S, S, A]:
-  //   override def modifyMetadata(f: Metadata => Metadata): Branch[S, S, A] = copy(metadata = f(metadata))
-  //   override def mapK[S1[a] >: S[a], U[_]](fK: T1 ~> U): Branch[S, U, A] = ???
+  final private[otter] case class Root[S[_], T[_], A, B](
+      key: Reference.Constant[S, A],
+      value: Reference[T, B],
+      metadata: Metadata
+  ) extends Branch[S, T, B]:
+    override def modifyMetadata(f: Metadata => Metadata): Branch[S, T, B] = copy(metadata = f(metadata))
+    override def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Branch[S, U, B] = copy(value = value.mapK(fK))
--- modules/core/src/main/scala/io/taig/otter/Union.scala
+++ modules/core/src/main/scala/io/taig/otter/Union.scala
@@ -1,28 +1,42 @@
 package io.taig.otter
 
-import cats.data.Chain
 import cats.syntax.all.*
 import cats.~>
+import cats.data.NonEmptyChain
 
-sealed abstract class Union[+S[_], +T[_], A]:
+sealed abstract class Union[+S[_], A] extends Product with Serializable:
+  def codecs: NonEmptyChain[Reference[S, ?]]
+  
   def metadata: Metadata
-  def modifyMetadata(f: Metadata => Metadata): Union[S, T, A]
-  def branches: Chain[Branch[S, T, ?]]
-  def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Union[S, U, A]
+  def modifyMetadata(f: Metadata => Metadata): Union[S, A]
 
-  final def imap[B](f: A => B)(g: B => A): Union[S, T, B] = ???
+  final def imap[B](f: A => B)(g: B => A): Union[S, B] = Union.Modify(self = this, f, g)
 
-  final def orElse[S1[a] >: S[a], T1[a] >: T[a], B](codec: Union[S1, T1, B]): Union[S1, T1, Either[A, B]] = ???
+  final def orElse[S1[a] >: S[a], B](codec: Union[S1, B]): Union[S1, Either[A, B]] =
+    Union.OrElse(left = this, right = codec, metadata = Metadata.Empty)
+
+  def mapK[S1[a] >: S[a], T[_]](fK: S1 ~> T): Union[T, A]
 
 object Union:
-  final private[otter] case class Modify[S[_], T[_], A, B](self: Union[S, T, A], f: A => B, g: B => A)
-      extends Union[S, T, B]:
-    export self.{branches, metadata}
-    override def modifyMetadata(f: Metadata => Metadata): Union[S, T, B] = copy(self = self.modifyMetadata(f))
-    override def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Union[S, U, B] = copy(self = self.mapK(fK))
+  final private[otter] case class Modify[S[_], A, B](self: Union[S, A], f: A => B, g: B => A)
+      extends Union[S, B]:
+    export self.{codecs, metadata}
+    override def modifyMetadata(f: Metadata => Metadata): Union[S, B] = copy(self = self.modifyMetadata(f))
+    override def mapK[S1[a] >: S[a], T[_]](fK: S1 ~> T): Union[T, B] = copy(self = self.mapK(fK))
 
-  final private[otter] case class Root[S[_], T[_], A](branch: Branch[S, T, A], metadata: Metadata)
-      extends Union[S, T, A]:
-    override def modifyMetadata(f: Metadata => Metadata): Union[S, T, A] = copy(metadata = f(metadata))
-    override def branches: Chain[Branch[S, T, ?]] = Chain.one(branch)
-    override def mapK[T1[a] >: T[a], U[_]](fK: T1 ~> U): Union[S, U, A] = copy(branch = branch.mapK(fK))
+  final private[otter] case class OrElse[S[_], A, B](
+      left: Union[S, A],
+      right: Union[S, B],
+      metadata: Metadata
+  ) extends Union[S, Either[A, B]]:
+    override def codecs: NonEmptyChain[Reference[S, ?]] = left.codecs ++ right.codecs
+    override def modifyMetadata(f: Metadata => Metadata): Union[S, Either[A, B]] =
+      copy(metadata = f(metadata))
+    override def mapK[S1[a] >: S[a], T[_]](fK: S1 ~> T): Union[T, Either[A, B]] =
+      copy(left = left.mapK(fK), right = right.mapK(fK))
+
+  final private[otter] case class Root[S[_], A](codec: Reference[S, A], metadata: Metadata)
+      extends Union[S, A]:
+    override def codecs: NonEmptyChain[Reference[S, ?]] = NonEmptyChain.one(codec)
+    override def modifyMetadata(f: Metadata => Metadata): Union[S, A] = copy(metadata = f(metadata))
+    override def mapK[S1[a] >: S[a], T[_]](fK: S1 ~> T): Union[T, A] = copy(codec = codec.mapK(fK))
