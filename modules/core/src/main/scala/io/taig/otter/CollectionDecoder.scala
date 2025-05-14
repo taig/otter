fc32103ec9d7bae23ab67aa3e7e4d9fd57923f35
Niklas Klein <mail@taig.io>
2025-05-14 09:39:51 +0200

    CollectionEncoder/Decoder

--- modules/core/src/main/scala/io/taig/otter/CollectionDecoder.scala
+++ modules/core/src/main/scala/io/taig/otter/CollectionDecoder.scala
@@ -4,8 +4,8 @@ import cats.data.Validated
 import cats.syntax.all.*
 import io.taig.otter.schema.Collection
 
-final class CollectionDecoder[S[_], T](decoder: Decoder[S, T]):
-  def apply[A](schema: Collection[S, A], values: Seq[T]): Validated[Violations, A] = schema match
+final class CollectionDecoder[S[_], T](decoder: Decoder[S, T]) extends Decoder[Collection[S, *], Seq[T]]:
+  override def apply[A](schema: Collection[S, A], values: Seq[T]): Validated[Violations, A] = schema match
     case Collection.Indexed(schema, _, _, _, _) =>
       values.toVector.zipWithIndex
         .traverse((value, index) => decoder(schema = schema.value, value).leftMap(index /: _))
---
+++ modules/core/src/main/scala/io/taig/otter/CollectionEncoder.scala
@@ -0,0 +1,11 @@
+package io.taig.otter
+
+import scala.annotation.tailrec
+import io.taig.otter.schema.Collection
+
+final class CollectionEncoder[S[_], T](encoder: Encoder[S, T]) extends Encoder[Collection[S, *], Seq[T]]:
+  @tailrec
+  override def apply[A](schema: Collection[S, A], a: A): Seq[T] = schema match
+    case Collection.Indexed(schema, _, _, _, _) => a.map(encoder(schema = schema.value, _))
+    case Collection.Linked(schema, _, _, _, _)  => a.map(encoder(schema = schema.value, _))
+    case Collection.Modify(self, _, g)          => apply(schema = self, g(a))
