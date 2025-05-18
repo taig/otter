96e24c02425580288612d4840219eb542b28ba42
Niklas Klein <mail@taig.io>
2025-05-18 13:54:00 +0200

    Path codecs

--- modules/http/src/main/scala/io/taig/otter/http/codec/PathDataDecoder.scala
+++ modules/http/src/main/scala/io/taig/otter/http/codec/PathDataDecoder.scala
@@ -1,21 +1,31 @@
-// package io.taig.otter.http.codec
+package io.taig.otter.http.codec
 
-// import cats.data.Chain
-// import cats.data.Validated
-// import cats.syntax.all.*
-// import io.taig.otter.Data
-// import io.taig.otter.Violation
-// import io.taig.otter.Violations
+import cats.data.Chain
+import cats.data.Validated
+import cats.syntax.all.*
+import io.taig.otter.Data
+import io.taig.otter.Violation
+import io.taig.otter.Violations
+import io.taig.otter.codec.Decoder
+import io.taig.otter.http.Path
 
-// object PathDataDecoder:
-//   object Remainders:
-//     def apply[A](path: Path[A], data: Path.Data): Validated[Violations, (Path.Data, A)] = path match
-//       case Path.Empty              => (data, ()).valid
-//       case Path.Modify(self, f, _) => apply(path = self, data).map(_.map(f))
-//       case Path.Root(segment) =>
-//         data.uncons match
-//           case Some((head, tail)) => SegmentParser(segment, value = head).tupleLeft(tail)
-//           case None               => Violations.rootNec(Violation.tpe(name = "segment", actual = Data.Null)).invalid
-//       case Path.Zip(left, right) =>
-//         apply(path = left, data).andThen: (data, a) =>
-//           apply(path = right, data).map(_.tupleLeft(a))
+object PathDataDecoder extends Decoder.Remainding[Path, Path.Data]:
+  def decodeRemainding[A](path: Path[A], data: Path.Data): Validated[Violations, (Path.Data, A)] = path match
+    case Path.Empty              => (data, ()).valid
+    case Path.Modify(self, f, _) => decodeRemainding(path = self, data).map(_.map(f))
+    case Path.Static(name) =>
+      data.uncons match
+        case Some((head, tail)) =>
+          Validated.cond(
+            test = head === name,
+            (tail, ()),
+            Violations.rootNec(Violation.equal(reference = name, actual = head))
+          )
+        case None => Violations.rootNec(Violation.equal(reference = name, actual = Data.Null)).invalid
+    case Path.Root(segment) =>
+      data.uncons match
+        case Some((head, tail)) => ParameterParser.decode(segment, value = head).tupleLeft(tail)
+        case None               => Violations.rootNec(Violation.tpe(name = "parameter", actual = Data.Null)).invalid
+    case Path.Zip(left, right) =>
+      decodeRemainding(path = left, data).andThen: (data, a) =>
+        decodeRemainding(path = right, data).map(_.tupleLeft(a))
