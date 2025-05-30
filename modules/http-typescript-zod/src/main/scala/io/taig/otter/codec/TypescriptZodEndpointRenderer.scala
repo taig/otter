b1fb9c5757cf1ebb5bae9252144b11f20b6acdf7
Niklas Klein <mail@taig.io>
2025-05-30 18:23:24 +0200

    Fix ts type

--- modules/http-typescript-zod/src/main/scala/io/taig/otter/codec/TypescriptZodEndpointRenderer.scala
+++ modules/http-typescript-zod/src/main/scala/io/taig/otter/codec/TypescriptZodEndpointRenderer.scala
@@ -23,7 +23,7 @@ object TypescriptZodEndpointRenderer:
       name = function(endpoint)
       input <- input(request = endpoint.request).map(_.definition(s"${name.capitalize}Input"))
       output <- output(endpoint.response).map(_.definition(s"${name.capitalize}Output"))
-      handle = s"""(code, headers, bod) =>
+      handle = s"""(code, headers, body) =>
                   |  body().then((value) => ${output.name}.parse({ code, value }))""".stripMargin
       fields = Chain(
         ("method", s"\"${endpoint.request.method}\""),
