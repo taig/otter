9df0d7229393258b71b37acf33350739ba437283
Niklas Klein <mail@taig.io>
2026-01-17 20:53:28 +0100

    Value.Branch

--- modules/core/src/main/scala/io/taig/otter/Value.scala
+++ modules/core/src/main/scala/io/taig/otter/Value.scala
@@ -16,23 +16,27 @@ object Value:
     def self: Annotation[Value.Read.Of[A]]
 
   object Read:
-    type Of[+A] = Self.Constant.Read[Value.Primitive.Text.Read, A] | Self.Primitive.Read[Value, A]
+    type Of[+A] = Self.Constant.Read[Value.Primitive.Text.Read, A] | Self.Primitive.Read[Value, A] |
+      Self.Union.Read[Value.Branch.Read, A]
 
     given Functor[Value.Read]:
       override def map[A, B](value: Value.Read[A])(f: A => B): Value.Read[B] = value match
         case value: Value.Constant.Read[A]       => value.map(f)
         case value: Value.Primitive.Text.Read[A] => value.map(f)
+        case value: Value.Union.Read[A]          => value.map(f)
 
   sealed trait Write[-A]:
     def self: Annotation[Value.Write.Of[A]]
 
   object Write:
-    type Of[-A] = Self.Constant.Write[Value.Primitive.Text.Write, A] | Self.Primitive.Write[Value.Write, A]
+    type Of[-A] = Self.Constant.Write[Value.Primitive.Text.Write, A] | Self.Primitive.Write[Value.Write, A] |
+      Self.Union.Write[Value.Branch.Write, A]
 
     given Contravariant[Value.Write]:
       override def contramap[A, B](value: Value.Write[A])(f: B => A): Value.Write[B] = value match
         case value: Value.Constant.Write[A]       => value.contramap(f)
         case value: Value.Primitive.Text.Write[A] => value.contramap(f)
+        case value: Value.Union.Write[A]          => value.contramap(f)
 
   sealed abstract class Constant[A] extends Value[A], Value.Constant.Read[A], Value.Constant.Write[A]:
     override def self: Annotation[Self.Constant[Value.Primitive.Text, A]]
@@ -130,9 +134,83 @@ object Value:
           (annotation: Annotation[Self.Primitive.Text[A]]) => Text(annotation)
         )([A] => (value: Value.Primitive.Text[A]) => value.self)
 
-  type Of[A] = Self.Constant[Value.Primitive.Text, A] | Self.Primitive[Value, A]
+  sealed abstract class Union[A] extends Value[A], Value.Union.Read[A], Value.Union.Write[A]:
+    override def self: Annotation[Self.Union[Value.Branch, A]]
+
+  object Union:
+    sealed trait Read[+A] extends Value.Read[A]:
+      override def self: Annotation[Self.Union.Read[Value.Branch.Read, A]]
+
+    object Read:
+      def apply[A](annotation: Annotation[Self.Union.Read[Value.Branch.Read, A]]): Value.Union.Read[A] =
+        new Read[A]:
+          override def self: Self.Annotation[Self.Union.Read[Value.Branch.Read, A]] = annotation
+
+      given Functor[Value.Union.Read] =
+        Functor[[a] =>> Annotation[Self.Union.Read[Value.Branch.Read, a]]].imapK([A] =>
+          (self: Annotation[Self.Union.Read[Value.Branch.Read, A]]) => Read(self)
+        )([A] => (value: Value.Union.Read[A]) => value.self)
+
+    sealed trait Write[-A] extends Value.Write[A]:
+      override def self: Annotation[Self.Union.Write[Value.Branch.Write, A]]
+
+    object Write:
+      def apply[A](annotation: Annotation[Self.Union.Write[Value.Branch.Write, A]]): Value.Union.Write[A] =
+        new Write[A]:
+          override def self: Self.Annotation[Self.Union.Write[Value.Branch.Write, A]] = annotation
+
+      given Contravariant[Value.Union.Write] =
+        Contravariant[[a] =>> Annotation[Self.Union.Write[Value.Branch.Write, a]]].imapK([A] =>
+          (annotation: Annotation[Self.Union.Write[Value.Branch.Write, A]]) => Write(annotation)
+        )([A] => (value: Value.Union.Write[A]) => value.self)
+
+    def apply[A](annotation: Annotation[Self.Union[Value.Branch, A]]): Value.Union[A] =
+      new Union[A]:
+        override def self: Self.Annotation[Self.Union[Value.Branch, A]] = annotation
+
+    given Invariant[Value.Union] =
+      Invariant[[a] =>> Annotation[Self.Union[Value.Branch, a]]].imapK([A] =>
+        (annotation: Annotation[Self.Union[Value.Branch, A]]) => Union(annotation)
+      )([A] => (value: Value.Union[A]) => value.self)
+
+  final case class Branch[A](self: Annotation[Self.Branch[Value, A]])
+      extends Value.Branch.Read[A],
+        Value.Branch.Write[A]
+
+  object Branch:
+    sealed trait Read[+A]:
+      def self: Annotation[Self.Branch.Read[Value.Read, A]]
+
+    object Read:
+      def apply[A](annotation: Annotation[Self.Branch.Read[Value.Read, A]]): Value.Branch.Read[A] = new Read[A]:
+        override def self: Self.Annotation[Self.Branch.Read[Value.Read, A]] = annotation
+
+      given Functor[Value.Branch.Read] = Functor[[a] =>> Annotation[Self.Branch.Read[Value.Read, a]]]
+        .imapK([A] => (self: Annotation[Self.Branch.Read[Value.Read, A]]) => Read(self))([A] =>
+          (value: Value.Branch.Read[A]) => value.self
+        )
+
+    sealed trait Write[-A]:
+      def self: Annotation[Self.Branch.Write[Value.Write, A]]
+
+    object Write:
+      def apply[A](annotation: Annotation[Self.Branch.Write[Value.Write, A]]): Value.Branch.Write[A] = new Write[A]:
+        override def self: Self.Annotation[Self.Branch.Write[Value.Write, A]] = annotation
+
+      given Contravariant[Value.Branch.Write] =
+        Contravariant[[a] =>> Annotation[Self.Branch.Write[Value.Write, a]]].imapK([A] =>
+          (annotation: Annotation[Self.Branch.Write[Value.Write, A]]) => Write(annotation)
+        )([A] => (value: Value.Branch.Write[A]) => value.self)
+
+    given Invariant[Value.Branch] = Invariant[[a] =>> Annotation[Self.Branch[Value, a]]]
+      .imapK([A] => (annotation: Annotation[Self.Branch[Value, A]]) => Branch(annotation))([A] =>
+        (value: Value.Branch[A]) => value.self
+      )
+
+  type Of[A] = Self.Constant[Value.Primitive.Text, A] | Self.Primitive[Value, A] | Self.Union[Value.Branch, A]
 
   given Invariant[Value]:
     override def imap[A, B](value: Value[A])(f: A => B)(g: B => A): Value[B] = value match
       case value: Value.Constant[A]       => value.imap(f)(g)
       case value: Value.Primitive.Text[A] => value.imap(f)(g)
+      case value: Value.Union[A]          => value.imap(f)(g)
