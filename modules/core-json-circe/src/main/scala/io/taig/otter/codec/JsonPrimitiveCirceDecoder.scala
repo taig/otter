857f4fdd93e232e334aebbb7fbf6147a2a368e2f
Niklas Klein <mail@taig.io>
2026-01-17 17:27:03 +0100

    Fully implement primitive json decoder

--- modules/core-json-circe/src/main/scala/io/taig/otter/codec/JsonPrimitiveCirceDecoder.scala
+++ modules/core-json-circe/src/main/scala/io/taig/otter/codec/JsonPrimitiveCirceDecoder.scala
@@ -8,6 +8,7 @@ import io.taig.otter.Json
 import io.taig.otter.Primitive
 import io.taig.otter.Violations
 import io.taig.otter.typeOf
+import io.taig.data.circe.*
 import io.taig.validation.Violation
 
 object JsonPrimitiveCirceDecoder extends Decoder[Json.Primitive.Read, CirceJson]:
@@ -16,21 +17,27 @@ object JsonPrimitiveCirceDecoder extends Decoder[Json.Primitive.Read, CirceJson]
 
   def decode[A](schema: Primitive.Read[Json.Primitive.Read, A], json: CirceJson): Validated[Violations, A] =
     schema match
-      case Primitive.Boolean.Modify(self, f, _) => decode(schema = self, json).map(f)
-      case Primitive.Boolean.Root               =>
+      case Primitive.Boolean.Modify(self, f, _)   => decode(schema = self, json).map(f)
+      case Primitive.Boolean.Read.Modify(self, f) => decode(schema = self, json).map(f)
+      case Primitive.Boolean.Root                 =>
         json.asBoolean
           .toValid(
             Violation(constraint = Constraint.Generic.Type(name = "boolean"), actual = typeOf(json), hint = none)
           )
           .leftMap(Violations.apply)
-      case Primitive.Coerce.Boolean.Root(schema) =>
+      case Primitive.Coerce.Boolean.Modify(self, f, _)   => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Boolean.Read.Modify(self, f) => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Boolean.Root(schema)         =>
         val value = json.withString:
           case "true"  => CirceJson.fromBoolean(true)
           case "false" => CirceJson.fromBoolean(false)
           case value   => CirceJson.fromString(value)
 
         decode(schema = schema.value, value)
-      case Primitive.Coerce.Number.Root(schema) =>
+      case Primitive.Coerce.Modify(self, f, _)          => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Number.Modify(self, f, _)   => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Number.Read.Modify(self, f) => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Number.Root(schema)         =>
         val value = json.withString: value =>
           schema.value.self.self match
             case Primitive.Number.Int(_) =>
@@ -50,6 +57,15 @@ object JsonPrimitiveCirceDecoder extends Decoder[Json.Primitive.Read, CirceJson]
                 .catchOnly[NumberFormatException](CirceJson.fromBigDecimal(BigDecimal(value)))
                 .getOrElse(CirceJson.fromString(value))
             case _ => CirceJson.fromString(value)
+
+        decode(schema = schema.value, value)
+      case Primitive.Coerce.Read.Modify(self, f)      => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Text.Modify(self, f, _)   => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Text.Read.Modify(self, f) => decode(schema = self, json).map(f)
+      case Primitive.Coerce.Text.Root(schema)         =>
+        val value = json
+          .withBoolean(value => CirceJson.fromString(String.valueOf(value)))
+          .withNumber(value => CirceJson.fromString(value.toString))
 
         decode(schema = schema.value, value)
       case Primitive.Number.BigDecimal(validation) =>
@@ -96,9 +112,9 @@ object JsonPrimitiveCirceDecoder extends Decoder[Json.Primitive.Read, CirceJson]
           .leftMap(Violation(_, actual = typeOf(json), hint = none))
           .leftMap(Violations.apply)
           .andThen(input => validation.validate(input).toInvalid(input).leftMap(Violations.apply))
-      case Primitive.Number.Modify(self, f, _) => decode(schema = self, json).map(f)
-      case Primitive.Text.Modify(self, f, _)   => decode(schema = self, json).map(f)
-      case Primitive.Text.Codec(_, parse, _)   =>
+      case Primitive.Number.Modify(self, f, _)   => decode(schema = self, json).map(f)
+      case Primitive.Number.Read.Modify(self, f) => decode(schema = self, json).map(f)
+      case Primitive.Text.Codec(name, parse, _)  =>
         json.asString
           .toValid(Constraint.Generic.Type(name = "string"))
           .leftMap(Violation(_, actual = typeOf(json), hint = none))
@@ -107,8 +123,24 @@ object JsonPrimitiveCirceDecoder extends Decoder[Json.Primitive.Read, CirceJson]
             parse(input).toValidated
               .leftMap: error =>
                 Violation(
-                  constraint = Constraint.Generic.Type(name = "string"),
-                  actual = typeOf(json),
+                  constraint = Constraint.Generic.Type(name),
+                  actual = json.toData,
+                  hint = error.some
+                )
+              .leftMap(Violations.apply)
+      case Primitive.Text.Modify(self, f, _)       => decode(schema = self, json).map(f)
+      case Primitive.Text.Read.Modify(self, f)     => decode(schema = self, json).map(f)
+      case Primitive.Text.Read.Parser(name, parse) =>
+        json.asString
+          .toValid(Constraint.Generic.Type(name = "string"))
+          .leftMap(Violation(_, actual = typeOf(json), hint = none))
+          .leftMap(Violations.apply)
+          .andThen: input =>
+            parse(input).toValidated
+              .leftMap: error =>
+                Violation(
+                  constraint = Constraint.Generic.Type(name),
+                  actual = json.toData,
                   hint = error.some
                 )
               .leftMap(Violations.apply)
