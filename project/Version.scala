014abc318d85f7604903367f40cfe612b1c4eeae
Niklas Klein <mail@taig.io>
2026-09-02 11:28:34 +0200

    Upgrade to data 0.0.4

--- project/Version.scala
+++ project/Version.scala
@@ -1,8 +1,8 @@
-object Version {
+object Version:
   val Cats = "2.13.0"
   val CatsParse = "1.1.0"
   val Circe = "0.14.16"
-  val Data = "0.0.3"
+  val Data = "0.0.4"
   val EnumerationExt = "0.6.0"
   val Fs2Data = "1.14.1"
   val Java = "17"
@@ -10,4 +10,3 @@ object Version {
   val ScalaJavaTime = "2.7.0"
   val Validation = "0.0.0+13-7bc8c23a-SNAPSHOT"
   val Zio = "2.1.26"
-}
